a0f5ed49e6511dc9b93199190c00e591c480c5e6
Himanshu Sharma <himanshusharma12283@gmail.com>
2025-04-02 20:37:04 +0530

    Time: 4 ms (18.53%) | Memory: 17.3 MB (18.72%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-      int n=grid.size();
-      int m=grid[0].size();
-      queue<pair<pair<int,int>,int>>q;
-      vector<vector<int>>vis(n,vector<int>(m,0));
+     int n=grid.size();
+     int m=grid[0].size();
 
-      for(int i=0;i<n;i++){
+     queue<pair<pair<int,int>,int>>q;
+      vector<vector<int>>vis(n,vector<int>(m,0));
+    for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
             if(grid[i][j]==2){
+                vis[i][j]==2;
                 q.push({{i,j},0});
-                vis[i][j]=2;
             }
         }
-      }
+    }
 
-      int tm=0;
-      vector<int>drow={-1,0,+1,0};
-      vector<int>dcol={0,+1,0,-1};
+    int time=0;
+    vector<int>drow={-1,0,1,0};
+    vector<int>dcol={0,1,0,-1};
 
-      while(!q.empty()){
+    while(!q.empty()){
         int r=q.front().first.first;
-        int c=q.front().first.second;
+        int c=q.front().first.second;;
         int t=q.front().second;
-        tm=max(tm,t);
         q.pop();
-
+        time=max(time,t);
         for(int i=0;i<4;i++){
             int nrow=r+drow[i];
             int ncol=c+dcol[i];
@@ -35,14 +34,14 @@ public:
                 vis[nrow][ncol]=2;
             }
         }
-      }
-      for(int i=0;i<n;i++){
+    }
+
+    for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
-            if(vis[i][j]!=2 && grid[i][j]==1){
-                return -1;
-            }
+          if(vis[i][j]!=2 && grid[i][j]==1){
+            return -1;
+          }
         }
-      }
-      return tm;  
+    }return time;
     }
 };
