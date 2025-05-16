32d0a10b662e7da493478e21f5d3648592b37def
Himanshu Sharma <himanshusharma12283@gmail.com>
2025-05-16 23:46:58 +0530

    Time: 35 ms (40.93%) | Memory: 14.1 MB (62.79%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -4,10 +4,12 @@ public:
         int n=nums.size();
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
-                if(nums[i]+nums[j]==target){
+                if((nums[i]+nums[j])==target){
                     return {i,j};
+                    break;
                 }
             }
-        }return {-1,-1};
+        }
+        return {};
     }
 };
