76f90162fc4d28f5983e05389fa3265bf96241e9
Himanshu Sharma <himanshusharma12283@gmail.com>
2025-07-17 20:11:54 +0530

    Time: 2 ms (4.53%) | Memory: 13.3 MB (5.82%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -10,16 +10,15 @@
  * };
  */
 class Solution {
-    
 public:
- vector<int> result; 
+vector<int>ans;
     vector<int> preorderTraversal(TreeNode* root) {
-     if(root==NULL){
-        return result;
-     }
-     result.push_back(root->val);
-     preorderTraversal(root->left);
-     preorderTraversal(root->right);
- return result;
+      if(root==NULL){
+        return ans;
+      } 
+      ans.push_back(root->val);
+      preorderTraversal(root->left);
+      preorderTraversal(root->right);
+      return ans; 
     }
 };
