6896e005b8fa76b899248bb57cba9c66abe92a67
Himanshu Sharma <himanshusharma12283@gmail.com>
2025-07-23 21:49:30 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (83.42%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isValid(string s) {
+    if(s.size()%2!=0) return false;
+
+    stack<char>st;
+    
+    for(int i=0;i<s.size();i++){
+
+    if(s[i]=='[' || s[i]=='{' || s[i]=='('){
+        st.push(s[i]);
+    }
+    else{
+
+        if(st.empty())  return false;
+
+            char ch=st.top();
+            st.pop();
+
+            if((s[i]!=']' && ch=='[') ||
+               (s[i]!='}' && ch=='{') || 
+               (s[i]!=')' && ch=='(')){
+                return false;
+            }
+
+        }
+    }
+    return st.empty(); 
+    }
+};
