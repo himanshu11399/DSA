17b35932f762b68b7fa86eec9f03f8516423ee61
Himanshu Sharma <himanshusharma12283@gmail.com>
2025-07-27 01:10:48 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (51.80%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,13 +1,7 @@
 class Solution {
 public:
 
-void makerow(vector<vector<int>>&matrix,int m,int i){
-    for(int j=0;j<m;j++){
-        if(matrix[i][j]!=0){
-       matrix[i][j]=0;
-    }
-    }
-}
+
 void makecol(vector<vector<int>>&matrix,int n,int j){
 for(int i=0;i<n;i++){
     if(matrix[i][j]!=0){
@@ -35,11 +29,17 @@ for(int i=0;i<n;i++){
         }
 
         for(int i=0;i<row.size();i++){
-            makerow(matrix,c,row[i]);
+            for(int j=0;j<c;j++){
+             matrix[row[i]][j]=0;
+    
+    }
         
         }
         for(int i=0;i<row.size();i++){
-            makecol(matrix,r,col[i]);
+           for(int j=0;j<r;j++){
+           matrix[j][col[i]]=0;
+    }
+    
         }
 
 
