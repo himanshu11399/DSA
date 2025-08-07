07d65e82bd8d5d0176537eef715761b225c8ddc5
Himanshu Sharma <himanshusharma12283@gmail.com>
2025-08-08 01:00:47 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (73.91%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,8 +1,19 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s==t;
+        if(s.size()!=t.size()) return false;
+       vector<int>arr(26,0);
+       for(int i=0;i<s.size();i++){
+           arr[s[i]-'a']++;
+       }
+
+       for(char &ch:t){
+        arr[ch-'a']--;
+       }
+
+       for(int i=0;i<arr.size();i++){
+        if(arr[i]>0) return false;
+       }
+       return true;
     }
 };
