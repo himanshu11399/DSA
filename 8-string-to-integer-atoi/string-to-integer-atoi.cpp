b9d16290372eaf0d367451afa96fc11473282258
Himanshu Sharma <himanshusharma12283@gmail.com>
2025-08-10 00:14:27 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (55.87%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int i=0;
+        int n=s.size();
+
+        while(i<n && s[i]==' ') i++;
+
+        int sign=1;
+        if(i<n && s[i]=='-'){
+            sign=-1;
+            i++;
+        }else if(i<n && s[i]=='+'){
+            i++;
+        }
+
+         long long ans=0;
+         while(i<n && isdigit(s[i])){
+            ans=ans*10+(s[i]-'0');
+            
+            if(ans*sign>INT_MAX) return INT_MAX;
+            if(ans*sign<INT_MIN) return INT_MIN;
+            i++;
+         }
+
+         return (int)(ans*sign);
+
+    }
+};
