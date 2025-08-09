3ebaae5726721ee0710760943935f0a32a4f035d
Himanshu Sharma <himanshusharma12283@gmail.com>
2025-08-10 01:25:31 +0530

    Time: 1 ms (54.36%) | Memory: 10.5 MB (60.26%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     string reverseWords(string s) {
+        int n=s.size();
         string ans;
         reverse(s.begin(),s.end());
 
         for(int i=0;i<s.size();i++){
-
-            string word;
-            while(i<s.size() && s[i]!=' '){
+             string word;
+             while(i<n && s[i]!=' '){
                 word+=s[i];
                 i++;
-            }
-            reverse(word.begin(),word.end());
-            if(word.size()>0){ 
-                ans+=' '+word;
-            }
-
+             }
+             reverse(word.begin(),word.end());
+             if(word.size()>0){
+                ans+=" "+word;
+             }
         }
-        return ans.substr(1);
+        return ans.substr(1,n+1);
     }
 };
