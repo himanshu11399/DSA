9c24ff23fa45d92ef73a552546fc7feb6a86beec
Himanshu Sharma <himanshusharma12283@gmail.com>
2025-08-27 10:02:19 +0530

    Time: 0 ms (100.00%) | Memory: 16.2 MB (69.01%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.cpp
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.cpp
@@ -15,10 +15,12 @@ public:
             return head;
         }
         ListNode* temp = head;
-
+        
         while (temp != NULL && temp->next != NULL) {
             if (temp->val == temp->next->val) {
+                ListNode*deleteNode=temp->next;
                 temp->next = temp->next->next;
+                delete deleteNode;
             } else {
                 temp = temp->next;
             }
