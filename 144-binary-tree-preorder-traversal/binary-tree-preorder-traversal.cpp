dc6045d120364db3e44e24d8f5c8624d18cffbf0
Himanshu Sharma <himanshusharma12283@gmail.com>
2025-09-05 00:07:43 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (5.75%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -6,19 +6,22 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-vector<int>ans;
+    vector<int> ans;
     vector<int> preorderTraversal(TreeNode* root) {
-      if(root==NULL){
+
+        if (root == NULL) {
+            return ans;
+        }
+        ans.push_back(root->val);
+        preorderTraversal(root->left);
+        preorderTraversal(root->right);
+
         return ans;
-      } 
-      ans.push_back(root->val);
-      preorderTraversal(root->left);
-      preorderTraversal(root->right);
-      return ans; 
     }
 };
