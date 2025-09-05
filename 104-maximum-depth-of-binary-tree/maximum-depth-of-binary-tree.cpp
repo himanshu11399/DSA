d2ee6422e804638289229650d7f39b1c4bbde7b4
Himanshu Sharma <himanshusharma12283@gmail.com>
2025-09-06 01:25:22 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (45.64%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -15,22 +15,8 @@ public:
         if(root==NULL){
             return 0;
         }
-      queue<pair<TreeNode*,int>>q;
-      q.push({root,1});
-      int currentlevel=1;
-      while(!q.empty()){
-        auto [curr,level]=q.front();
-        q.pop();
-        if(level>currentlevel){
-            currentlevel=level;
-        }
-        if(curr->left!=NULL){
-            q.push({curr->left,level+1});
-        }
-        if(curr->right!=NULL){
-            q.push({curr->right,level+1});
-        }
-      } 
-      return currentlevel; 
+        int left=maxDepth(root->left);
+        int right=maxDepth(root->right);
+        return max(left,right)+1;
     }
 };
