94bf6488049e7663118f7ec30b4406ba38776c10
Himanshu Sharma <himanshusharma12283@gmail.com>
2025-09-13 03:12:46 +0530

    Time: 27 ms (8.42%) | Memory: 75.8 MB (6.00%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -6,38 +6,38 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-      int preindex=0;
-       int n=preorder.size();
-       map<int,int>nodetoindex;
-       createmap(inorder,nodetoindex);
-      TreeNode*ans=solve(preorder,inorder,preindex,0,n-1,nodetoindex);
-      return ans;  
-    }
-    private:
-    TreeNode*solve(vector<int>preorder,vector<int>inorder,int& index,int start,int end
-    ,map<int,int>&nodetoindex){
-        int n=preorder.size();
-        if(index>=n || start>end)   return NULL;
-        int current=preorder[index];
-        index++;
-        TreeNode*root=new TreeNode(current);
-        int pos=nodetoindex[current];
-        
-        root->left=solve(preorder,inorder,index,start,pos-1,nodetoindex);
-        root->right=solve(preorder,inorder,index,pos+1,end,nodetoindex);
-        return root;
-
-    }
-    void createmap(vector<int>arr, map<int,int>&nodetoindex){
-        for(int i=0;i<arr.size();i++){
-            nodetoindex[arr[i]]=i;
+    int search(vector<int> arr, int st, int end, int val) {
+        for (int i = st; i <= end; i++) {
+            if (arr[i] == val) {
+                return i;
             }
         }
-    
+        return -1;
+    }
+    TreeNode* helper(vector<int>& preorder, vector<int>& inorder, int left,
+                     int right, int& preIdx) {
+
+        if (left > right)
+            return NULL;
+
+        TreeNode* newNode = new TreeNode(preorder[preIdx]);
+
+        int inIdx=search(inorder,left,right,preorder[preIdx]);
+        preIdx++;
+
+        newNode->left = helper(preorder, inorder, left, inIdx - 1, preIdx);
+        newNode->right =helper(preorder, inorder, inIdx + 1, right, preIdx);
+
+        return newNode;
+    }
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        int idx = 0;
+        return helper(preorder, inorder, 0, inorder.size()-1, idx);
+    }
 };
