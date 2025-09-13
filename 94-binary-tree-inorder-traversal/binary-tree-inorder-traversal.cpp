3985fd1d3f3f1ae50c9bfc971a7b6f1fa092e066
Himanshu Sharma <himanshusharma12283@gmail.com>
2025-09-13 22:25:55 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (63.79%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -14,12 +14,34 @@ class Solution {
 public:
     vector<int> ans;
     vector<int> inorderTraversal(TreeNode* root) {
-        if (root == NULL) {
+        if (root == NULL)
             return ans;
+
+        while (root) {
+
+            // left does not exist;
+            if (root->left == NULL) {
+                ans.push_back(root->val);
+                root = root->right;
+            } else { // if left is exist
+                TreeNode* curr = root->left;
+                while (curr->right &&
+                       curr->right != root) { // It is used to check the Left is
+                                              // traversed or not
+                    curr = curr->right;
+                }
+
+                // if left is traversed
+                if (curr->right == root) { 
+                    curr->right = NULL;
+                    ans.push_back(root->val);
+                    root = root->right;
+                } else {// If the left is not traversed
+                    curr->right = root;
+                    root = root->left;
+                }
+            }
         }
-        inorderTraversal(root->left);
-        ans.push_back(root->val);
-        inorderTraversal(root->right);
 
         return ans;
     }
