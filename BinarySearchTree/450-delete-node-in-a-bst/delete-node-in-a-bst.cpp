ca2d726e0fb32b6282fb59512a09059b569f0234
Himanshu Sharma <himanshusharma12283@gmail.com>
2025-09-14 18:14:06 +0530

    Time: 0 ms (100.00%) | Memory: 34.3 MB (65.21%) - LeetSync

---
+++ BinarySearchTree/450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+//Find the succ
+    TreeNode* findMin(TreeNode* node) {
+        while (node && node->left) {
+            node = node->left;
+        }
+        return node;
+    }
+
+    //Delete Node
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if (root == NULL)  //If root is empty
+            return NULL;
+
+        if (root->val > key) {  
+            root->left = deleteNode(root->left, key);
+        } else if (root->val < key) {
+            root->right = deleteNode(root->right, key);
+        } else {
+            if (!root->left) {
+                TreeNode* temp = root->right;
+                delete root;
+                return temp;
+            } else if (!root->right) {
+                TreeNode* temp = root->left;
+                delete root;
+                return temp;
+            } else {
+                TreeNode* succ = findMin(root->right);
+                root->val = succ->val;
+                root->right=deleteNode(root->right, succ->val);
+            }
+        }
+        return root;
+    }
+};
