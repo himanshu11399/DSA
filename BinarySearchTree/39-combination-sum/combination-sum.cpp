0c6ebd1156ad13d0d66aa5537c0fab937aef592c
Himanshu Sharma <himanshusharma12283@gmail.com>
2025-09-21 00:04:44 +0530

    Time: 3 ms (55.03%) | Memory: 14.1 MB (54.30%) - LeetSync

--- BinarySearchTree/39-combination-sum/combination-sum.cpp
+++ BinarySearchTree/39-combination-sum/combination-sum.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-set<vector<int>>ans;
+vector<vector<int>>ans;
 void solve(int i,vector<int>&nums,vector<int>&curr,int k){
     if(i==nums.size() || k<0){
         return;
     }
     if(k==0){
-        ans.insert(curr);
+        ans.push_back(curr);
         return;
     }
     
@@ -19,11 +19,6 @@ void solve(int i,vector<int>&nums,vector<int>&curr,int k){
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
        vector<int>arr;
        solve(0,candidates,arr,target);
-
-       vector<vector<int>>res;
-       for(auto it:ans){
-        res.push_back(it);
-       }
-       return res; 
+       return ans; 
     }
 };
