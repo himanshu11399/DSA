6fb50efac3d17411e9cecd00b84f753b078f83b5
Himanshu Sharma <himanshusharma12283@gmail.com>
2025-09-23 20:12:50 +0530

    Time: 246 ms (63.56%) | Memory: 10.7 MB (65.16%) - LeetSync

--- BinarySearchTree/79-word-search/word-search.cpp
+++ BinarySearchTree/79-word-search/word-search.cpp
@@ -1,34 +1,39 @@
 class Solution {
 public:
     int n, m;
-    vector<vector<int>> direction{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    vector<vector<int>> direction{{1, 0}, {-1, 0}, {0, 1}, {0, - 1}};
 
     bool solve(vector<vector<char>>& board, int i, int j, int idx, string& word) {
-        if (idx == word.size()) return true;
-        if (i < 0 || j < 0 || i >= m || j >= n) return false;
-        if (board[i][j] != word[idx]) return false;
+        if (idx == word.size()) {
+            return true;
+        }
+        if (i < 0 || j < 0 || i >= m || j >= n) {
+            return false;
+        }
+        if(board[i][j]!=word[idx]) return false;
 
         char temp = board[i][j];
-        board[i][j] = '$';  // mark visited
+        board[i][j] = '$';
 
         for (auto& it : direction) {
             if (solve(board, i + it[0], j + it[1], idx + 1, word)) {
-                board[i][j] = temp;  // restore before returning
+                board[i][j]=temp;
                 return true;
             }
         }
 
-        board[i][j] = temp;  // restore back
+        board[i][j] = temp;
         return false;
     }
-
     bool exist(vector<vector<char>>& board, string word) {
         m = board.size();
         n = board[0].size();
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (board[i][j] == word[0]) {
-                    if (solve(board, i, j, 0, word)) return true;
+                    if (solve(board, i, j, 0, word)) {
+                        return true;
+                    }
                 }
             }
         }
