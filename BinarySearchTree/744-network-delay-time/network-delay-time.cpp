c3a3be0eca00a1120c1695655d9328066c6e5fcd
Himanshu Sharma <himanshusharma12283@gmail.com>
2025-10-03 20:10:41 +0530

    Time: 101 ms (13.89%) | Memory: 48 MB (29.20%) - LeetSync

--- BinarySearchTree/744-network-delay-time/network-delay-time.cpp
+++ BinarySearchTree/744-network-delay-time/network-delay-time.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int src) {
-        vector<vector<pair<int, int>>> adj(n+1);
+        vector<vector<pair<int, int>>> adj(n + 1);
         //{v,wt}
         for (auto it : times) {
             adj[it[0]].push_back({it[1], it[2]});
@@ -12,15 +12,13 @@ public:
             pq;
         pq.push({0, src});
         //{dis,src}
-
-        vector<int> dist(n+1, 1e9);
+        vector<int> dist(n + 1, 1e9);
         dist[src] = 0;
         while (!pq.empty()) {
             auto it = pq.top();
             int cost = it.first;
             int node = it.second;
             pq.pop();
-
             for (auto i : adj[node]) {
                 int adjnode = i.first;
                 int wt = i.second;
@@ -30,14 +28,13 @@ public:
                 }
             }
         }
-
-       int maxdelay = 0;
-       for(int i=1;i<=n;i++){
-        if(dist[i]==1e9){
-            return -1;
+        int maxdelay = 0;
+        for (int i = 1; i <= n; i++) {
+            if (dist[i] == 1e9) {
+                return -1;
+            }
+            maxdelay = max(maxdelay, dist[i]);
         }
-        maxdelay=max(maxdelay,dist[i]);
-       }
-       return maxdelay;
+        return maxdelay;
     }
 };
