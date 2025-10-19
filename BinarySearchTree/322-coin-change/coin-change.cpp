c68dd10d9e4be2d847af55506940d6e2fcdc6266
Himanshu Sharma <himanshusharma12283@gmail.com>
2025-10-20 01:05:32 +0530

    Time: 27 ms (62.66%) | Memory: 19.1 MB (54.95%) - LeetSync

--- BinarySearchTree/322-coin-change/coin-change.cpp
+++ BinarySearchTree/322-coin-change/coin-change.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-int dp[10040];
-    int func(vector<int>& coins, int amount) {
+
+    int func(vector<int>& coins, int amount,vector<int>&dp) {
         if (amount == 0) return 0;
         if(dp[amount]!=-1) return dp[amount];
 
         int ans = INT_MAX;
         for (int coin : coins) {
             if (amount - coin >= 0) {
-                ans = min(ans + 0LL, func(coins, amount - coin) + 1LL);
+                ans = min(ans + 0LL, func(coins, amount - coin,dp) + 1LL);
             }
         }
         return dp[amount]=ans;
     }
     int coinChange(vector<int>& coins, int amount) {
-        memset(dp,-1,sizeof(dp));
-        int ans=func(coins,amount);
+        vector<int>dp(amount+1,-1);
+        int ans=func(coins,amount,dp);
         return ans==INT_MAX?-1:ans;
     }
 };
