bf685b14e3cb8bd2688991b75f52aa8ae103046d
Himanshu Sharma <himanshusharma12283@gmail.com>
2025-10-26 03:30:57 +0530

    Time: 0 ms (100.00%) | Memory: 21.7 MB (74.88%) - LeetSync

--- BinarySearchTree/57-insert-interval/insert-interval.cpp
+++ BinarySearchTree/57-insert-interval/insert-interval.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& interval, vector<int>& newInterval) {
-       int i=0;
-       while(i<interval.size()){
-        if(interval[i][1]<newInterval[0]){
-         i++;
-        }else if(interval[i][0]>newInterval[1]){
-            interval.insert(interval.begin()+i,newInterval);
-            return interval;
-        }else{
-            newInterval[0]=min(newInterval[0],interval[i][0]);
-            newInterval[1]=max(newInterval[1],interval[i][1]);
-            interval.erase(interval.begin()+i);
+    vector<vector<int>> insert(vector<vector<int>>& interval,
+                               vector<int>& newInterval) {
+        int i = 0;
+        vector<vector<int>> ans;
+        while (i < interval.size()) {
+            if (interval[i][1] < newInterval[0]) {
+                ans.push_back({interval[i][0], interval[i][1]});
+                i++;
+            } else if (interval[i][0] > newInterval[1]) {
+                ans.push_back({newInterval[0], newInterval[1]});
+                while(i<interval.size()){
+                ans.push_back({interval[i][0], interval[i][1]});
+                i++;
+                }
+                return ans;
+            } else {
+                newInterval[0] = min(newInterval[0], interval[i][0]);
+                newInterval[1] = max(newInterval[1], interval[i][1]);
+                i++;
+            }
         }
-       }
-       interval.push_back(newInterval);
-       return interval; 
+        ans.push_back({newInterval[0], newInterval[1]});
+        return ans;
     }
 };
