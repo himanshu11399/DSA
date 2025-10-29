304a561403e6278db8d99883a932bd1b0b535fb4
Himanshu Sharma <himanshusharma12283@gmail.com>
2025-10-30 04:01:47 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (56.86%) - LeetSync

--- BinarySearchTree/70-climbing-stairs/climbing-stairs.cpp
+++ BinarySearchTree/70-climbing-stairs/climbing-stairs.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
-   int fun(int n,vector<int>&dp){
-    if(n==0 || n==1) return 1;
-    if(n<0) return 0;
-    if(dp[n]!=-1) return dp[n];
-
-    int left=fun(n-1,dp);
-    int right=fun(n-2,dp);
-
-    return dp[n]=left+right;
-   }
     int climbStairs(int n) {
-        vector<int>dp(n+1,-1);
-        return fun(n,dp);
+       if(n<=1) return 1;
+       int a=1,b=1;
+       for(int i=2;i<=n;i++){
+          int c=a+b;
+          a=b;
+          b=c;
+       } 
+       return b;
     }
 };
