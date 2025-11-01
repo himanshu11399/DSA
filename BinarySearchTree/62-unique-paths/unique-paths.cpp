91d6fc270e196fe5a06b8f6cd37b5ffd83b2de0d
Himanshu Sharma <himanshusharma12283@gmail.com>
2025-11-02 04:01:14 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (41.26%) - LeetSync

--- BinarySearchTree/62-unique-paths/unique-paths.cpp
+++ BinarySearchTree/62-unique-paths/unique-paths.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 int solve(int i,int j,int m,int n,vector<vector<int>>&dp){
-    if(i<0 || j<0 || i>=m || j>=n) return 0;
+    if(i>=m || j>=n) return 0;
     if(i==m-1 && j==n-1 ) return 1;
     if(dp[i][j]!=-1) return dp[i][j];
     int R=solve(i,j+1,m,n,dp);
