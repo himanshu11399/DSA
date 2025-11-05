ae72a0cc0eeb401c4463c729b58ca5582bd84e69
Himanshu Sharma <himanshusharma12283@gmail.com>
2025-11-06 03:04:09 +0530

    Time: 15 ms (55.32%) | Memory: 25.2 MB (33.84%) - LeetSync

--- BinarySearchTree/494-target-sum/target-sum.cpp
+++ BinarySearchTree/494-target-sum/target-sum.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
-int solve(int i,vector<int>&nums,int target){
-   if(i==nums.size() && target==0){
-    return 1;
-   }
-   if(i>=nums.size() && target<0 ){
-    return 0;
-   }
-   if(i>=nums.size()) return 0;
+    vector<vector<int>> dp;
+    int solve(int i, vector<int>& nums, int target,int sum) {
+        if(target>sum) return 0;
+        if(i==nums.size()) return (target==0);
+        if (dp[i][target+sum] != -1) {
+            return dp[i][target+sum];
+        }
 
-   //Assign +
-   int add=solve(i+1,nums,target-nums[i]);
-   //Assign -
-   int sub=solve(i+1,nums,target+nums[i]);
+        // Assign +
+        int add = solve(i + 1, nums, target - nums[i],sum);
+        // Assign -
+        int sub = solve(i + 1, nums, target + nums[i],sum);
 
-   return add+sub; 
-}
+        return dp[i][target+sum] = (add + sub);
+    }
     int findTargetSumWays(vector<int>& nums, int target) {
-       return solve(0,nums,target); 
+        int totalsum=accumulate(nums.begin(),nums.end(),0);
+        if(totalsum<abs(target)) return 0;
+        int n = nums.size();
+        dp.assign(n, vector<int>(2*totalsum+1, -1));
+        return solve(0, nums,target,totalsum);
     }
 };
