5eeb33f3e04c47a56b0841f650e91449e58973c0
Himanshu Sharma <himanshusharma12283@gmail.com>
2025-11-09 20:19:01 +0530

    Time: 11 ms (74.08%) | Memory: 58.8 MB (87.82%) - LeetSync

--- BinarySearchTree/714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ BinarySearchTree/714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int solve(int i, int buy, vector<int>& prices, int charge) {
-        if (i == prices.size())
-            return 0;
-        if (dp[i][buy] != -1)
-            return dp[i][buy];
-        int profit = 0;
-        if (buy) {
-            int buyStock =
-                -(prices[i] + charge) + solve(i + 1, 0, prices, charge);
-            int ignoreBuy = solve(i + 1, 1, prices, charge);
-            profit = max(buyStock, ignoreBuy);
-        } else {
-            int sellStock = +prices[i] + solve(i + 1, 1, prices, charge);
-            int ignoresell = solve(i + 1, 0, prices, charge);
-            profit = max(sellStock, ignoresell);
-        }
-        return dp[i][buy] = profit;
-    }
+    // vector<vector<int>> dp;
+    // int solve(int i, int buy, vector<int>& prices, int charge) {
+    //     if (i == prices.size())
+    //         return 0;
+    //     if (dp[i][buy] != -1)
+    //         return dp[i][buy];
+    //     int profit = 0;
+    //     if (buy) {
+    //         int buyStock =
+    //             -(prices[i] + charge) + solve(i + 1, 0, prices, charge);
+    //         int ignoreBuy = solve(i + 1, 1, prices, charge);
+    //         profit = max(buyStock, ignoreBuy);
+    //     } else {
+    //         int sellStock = +prices[i] + solve(i + 1, 1, prices, charge);
+    //         int ignoresell = solve(i + 1, 0, prices, charge);
+    //         profit = max(sellStock, ignoresell);
+    //     }
+    //     return dp[i][buy] = profit;
+    // }
 
     int optimal(vector<int>& prices,int charges) {
         int n = prices.size();
