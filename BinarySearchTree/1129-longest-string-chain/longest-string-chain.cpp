9b78b3ce5f4e68cfad1f16719a619d7d98fdecf4
Himanshu Sharma <himanshusharma12283@gmail.com>
2025-11-14 00:37:59 +0530

    Time: 45 ms (59.25%) | Memory: 18.5 MB (95.49%) - LeetSync

---
+++ BinarySearchTree/1129-longest-string-chain/longest-string-chain.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool ispredecessor(string& a, string& b) {
+        if (b.size() != a.size() + 1)
+            return false;
+        int i = 0, j = 0;
+        while (j < b.size()) {
+            if (i < a.size() && a[i] == b[j]) {
+                i++;
+            }
+            j++;
+        }
+        return i == a.size();
+    }
+
+    int longestStrChain(vector<string>& words) {
+        sort(words.begin(), words.end(),
+             [](const string& a,const string& b) { return a.size() < b.size(); });
+
+            int n = words.size();
+        vector<int> dp(n, 1);
+        int lis = 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (ispredecessor(words[j], words[i])) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+            lis = max(lis, dp[i]);
+        }
+        return lis;
+    }
+};
