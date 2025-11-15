365c2d9c8938ac870f001a2b69c0bfe1fe8b56f9
Himanshu Sharma <himanshusharma12283@gmail.com>
2025-11-15 10:08:52 +0530

    Time: 13 ms (18.50%) | Memory: 21.4 MB (18.31%) - LeetSync

--- BinarySearchTree/1829-maximum-units-on-a-truck/maximum-units-on-a-truck.cpp
+++ BinarySearchTree/1829-maximum-units-on-a-truck/maximum-units-on-a-truck.cpp
@@ -1,10 +1,8 @@
 class Solution {
 public:
     int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
-        sort(boxTypes.begin(),
-             boxTypes.end(),[](vector<int> & a, vector<int> & b){
-                return a[1] >b[1];
-    });
+        sort(boxTypes.begin(), boxTypes.end(),
+             [](vector<int>& a, vector<int>& b) { return a[1] > b[1]; });
 
         int totalUnits = 0;
 
