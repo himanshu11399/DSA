f72ef80341c8a3122aef0ed0e80e928606c5f457
Himanshu Sharma <himanshusharma12283@gmail.com>
2025-11-20 01:08:06 +0530

    Time: 49 ms (17.83%) | Memory: 37.2 MB (23.97%) - LeetSync

--- BinarySearchTree/518-coin-change-ii/coin-change-ii.cpp
+++ BinarySearchTree/518-coin-change-ii/coin-change-ii.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
-int func(int idx,vector<int>&coins,int amount,vector<vector<int>>&dp){
-    if(amount==0) return 1;
-    if(idx==coins.size()) return 0;
-    if(dp[idx][amount]!=-1) return dp[idx][amount];
+    int func(int idx, vector<int>& coins, int amount, vector<vector<int>>& dp) {
+        if (amount == 0)
+            return 1;
+        if (idx == coins.size())
+            return 0;
+        if (dp[idx][amount] != -1)
+            return dp[idx][amount];
 
-    int ways=0;
-    if(coins[idx]<=amount){
-        ways+=func(idx,coins,amount-coins[idx],dp);
-        
+        int ways = 0;
+        if (coins[idx] <= amount) {
+            ways += func(idx, coins, amount - coins[idx], dp);
+        }
+        ways += func(idx + 1, coins, amount, dp);
+        return dp[idx][amount] = ways;
     }
-    ways+=func(idx+1,coins,amount,dp);
-    return dp[idx][amount]=ways;
-}
     int change(int amount, vector<int>& coins) {
-        vector<vector<int>>dp(coins.size()+1,vector<int>(amount+1,-1));
-        return func(0,coins,amount,dp);
+        vector<vector<int>> dp(coins.size() + 1, vector<int>(amount + 1, -1));
+        return func(0, coins, amount, dp);
     }
 };
