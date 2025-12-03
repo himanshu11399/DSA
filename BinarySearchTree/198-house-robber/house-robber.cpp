016f12cdf5cfe1248f283f8cf3b682f746d39f30
Himanshu Sharma <himanshusharma12283@gmail.com>
2025-12-04 02:23:51 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (45.08%) - LeetSync

--- BinarySearchTree/198-house-robber/house-robber.cpp
+++ BinarySearchTree/198-house-robber/house-robber.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
+vector<int>dp;
+int solve(int i,vector<int>&nums){
+    if(i>=nums.size()){
+        return 0;
+    }
+    if(dp[i]!=-1) return dp[i];
 
-     //Memoization
-    // int find(int i, vector<int>& nums, vector<int>& dp) {
-    //     if (i >= nums.size())
-    //         return 0;
-    //     if (dp[i] != -1)
-    //         return dp[i];
+    //take
+    int left=nums[i]+solve(i+2,nums);
 
-    //     int robnow = nums[i] + find(i + 2, nums,dp);
-    //     int skipnow = find(i + 1, nums,dp);
+    //not take
+    int right=solve(i+1,nums);
 
-    //     return dp[i] = max(robnow, skipnow);
-    // }
+    return dp[i]= max(left,right);
+}
     int rob(vector<int>& nums) {
-        // vector<int> dp(nums.size() + 1, -1);
-        // return find(0, nums, dp);
-
-        //Tabulation
-        int n=nums.size();
-        vector<int>dp(n+2,0);
-        for(int i=n-1;i>=0;i--){
-          dp[i]=max(nums[i]+dp[i+2],dp[i+1]);
-        }
-        return dp[0];
+        dp.assign(nums.size()+1,-1);
+       return solve(0,nums); 
     }
 };
