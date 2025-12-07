8eb7294e7228c9d19407d5a3e762961409bb26c2
Himanshu Sharma <himanshusharma12283@gmail.com>
2025-12-07 23:47:26 +0530

    Time: 27 ms (55.80%) | Memory: 27.3 MB (67.89%) - LeetSync

--- BinarySearchTree/1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ BinarySearchTree/1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,23 +1,35 @@
 class Solution {
 public:
-vector<vector<int>>dp;
-int solve(int i,int j,string& t1,string& t2){
-    if(i>=t1.size() || j>=t2.size()) return 0;
-    if(dp[i][j]!=-1) return dp[i][j];
-    
-    int take=0;
-    if(t1[i]==t2[j]){
-      take=1+solve(i+1,j+1,t1,t2);
-    }
-    int notake=max(solve(i+1,j,t1,t2),solve(i,j+1,t1,t2));
+    // vector<vector<int>>dp;
+    // int solve(int i,int j,string& t1,string& t2){
+    //     if(i>=t1.size() || j>=t2.size()) return 0;
+    //     if(dp[i][j]!=-1) return dp[i][j];
 
-    return dp[i][j]=max(take,notake);
+    //     int take=0;
+    //     if(t1[i]==t2[j]){
+    //       take=1+solve(i+1,j+1,t1,t2);
+    //     }
+    //     int notake=max(solve(i+1,j,t1,t2),solve(i,j+1,t1,t2));
 
-}
+    //     return dp[i][j]=max(take,notake);
+
+    // }
     int longestCommonSubsequence(string text1, string text2) {
-       int n=text1.size();
-       int m=text2.size();
-       dp.assign(n+1,vector<int>(m+1,-1));
-       return solve(0,0,text1,text2); 
+        int n = text1.size();
+        int m = text2.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= m; j++) {
+
+                if (text1[i - 1] == text2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+
+                dp[i][j] = max(dp[i][j], max(dp[i - 1][j], dp[i][j - 1]));
+            }
+        }
+        return dp[n][m];
     }
 };
