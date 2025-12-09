7bb7b172bfee287ffcbe43a3439b1d9acece7d56
Himanshu Sharma <himanshusharma12283@gmail.com>
2025-12-09 23:32:40 +0530

    Time: 8 ms (8.89%) | Memory: 19.7 MB (21.27%) - LeetSync

--- BinarySearchTree/122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ BinarySearchTree/122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,26 +1,44 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int solve(int i, int buy, vector<int>& prices) {
-        if (i >= prices.size())
-            return 0;
-        if (dp[i][buy] != -1)
-            return dp[i][buy];
-        int profit = 0;
-        if (buy) {
-            int buyit = -prices[i] + solve(i + 1, 0, prices);
-            int ignoreit = solve(i + 1, 1, prices);
-            profit = max(buyit, ignoreit);
-        } else {
-            int sellit = prices[i] + solve(i + 1, 1, prices);
-            int ignoreit = solve(i + 1, 0, prices);
-            profit = max(sellit, ignoreit);
-        }
-        return dp[i][buy] = profit;
-    }
+    // vector<vector<int>> dp;
+    // int solve(int i, int buy, vector<int>& prices) {
+    //     if (i >= prices.size())
+    //         return 0;
+    //     if (dp[i][buy] != -1)
+    //         return dp[i][buy];
+    //     int profit = 0;
+    //     if (buy) {
+    //         int buyit = -prices[i] + solve(i + 1, 0, prices);
+    //         int ignoreit = solve(i + 1, 1, prices);
+    //         profit = max(buyit, ignoreit);
+    //     } else {
+    //         int sellit = prices[i] + solve(i + 1, 1, prices);
+    //         int ignoreit = solve(i + 1, 0, prices);
+    //         profit = max(sellit, ignoreit);
+    //     }
+    //     return dp[i][buy] = profit;
+    // }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        dp.assign(n + 1, vector<int>(2, -1));
-        return solve(0, 1, prices);
+        // dp.assign(n + 1, vector<int>(2, -1));
+        // return solve(0, 1, prices);
+        vector<vector<int>> dp(n + 1, vector<int>(2, 0));
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                int profit = 0;
+                if (buy) {
+                    int buyit = -prices[i] + dp[i + 1][0];
+                    int ignoreit = dp[i + 1][1];
+                    profit = max(buyit, ignoreit);
+                } else {
+                    int sellit = prices[i] + dp[i + 1][1];
+                    int ignoreit = dp[i + 1][0];
+                    profit = max(sellit, ignoreit);
+                }
+                dp[i][buy] = profit;
+            }
+        }
+        return dp[0][1];
     }
 };
