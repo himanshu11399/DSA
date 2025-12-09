4803fd89ad818dcd9bfca47a4d76ae1ef68f55bd
Himanshu Sharma <himanshusharma12283@gmail.com>
2025-12-09 23:53:04 +0530

    Time: 400 ms (35.87%) | Memory: 211.6 MB (28.56%) - LeetSync

--- BinarySearchTree/123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ BinarySearchTree/123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,57 +1,27 @@
 class Solution {
 public:
-    // vector<vector<vector<int>>> dp;
-    // int solve(int i, int buy, vector<int>& prices, int limit) {
-    //     if (i >= prices.size() || limit <= 0)
-    //         return 0;
-    //     if (dp[i][buy][limit] != -1)
-    //         return dp[i][buy][limit];
-
-    //     int profit = 0;
-    //     if (buy) {
-    //         int buyStock = -prices[i] + solve(i + 1, 0, prices, limit);
-    //         int ignorebuy = solve(i + 1, 1, prices, limit);
-    //         profit = max(buyStock, ignorebuy);
-    //     } else {
-    //         int sellStock = +prices[i] + solve(i + 1, 1, prices, limit - 1);
-    //         int ignoresell = solve(i + 1, 0, prices, limit);
-    //         profit = max(sellStock, ignoresell);
-    //     }
-    //     return dp[i][buy][limit] = profit;
-    // }
-
-    int tabulation(vector<int>& prices) {
+    int maxProfit(vector<int>& prices) {
         int n = prices.size();
-         
-        vector<vector<int>>curr(2,vector<int>(3,0));
-        vector<vector<int>>next(2,vector<int>(3,0));
+        vector<vector<vector<int>>> dp(
+            n + 1, vector<vector<int>>(2, vector<int>(3, 0)));
 
         for (int i = n - 1; i >= 0; i--) {
-            for (int j = 0; j <= 1; j++) {
-                for (int k = 1; k <= 2; k++) {
+            for (int buy = 0; buy <= 1; buy++) {
+                for (int limit = 1; limit <= 2; limit++) {
                     int profit = 0;
-                    if (j) {
-                        int buyStock =
-                            -prices[i] + next[0][k];
-                        int ignorebuy =next[1][k];
-                        profit = max(buyStock, ignorebuy);
+                    if (buy) {
+                        int buyit = -prices[i] + dp[i + 1][0][limit];
+                        int ignoreit = dp[i + 1][1][limit];
+                        profit = max(buyit, ignoreit);
                     } else {
-                        int sellStock =
-                            +prices[i] + next[1][k-1];
-                        int ignoresell = next[0][k];
-                        profit = max(sellStock, ignoresell);
+                        int sellit = prices[i] + dp[i + 1][1][limit-1];
+                        int ignoreit = dp[i + 1][0][limit];
+                        profit = max(sellit, ignoreit);
                     }
-                    curr[j][k] = profit;
+                    dp[i][buy][limit] = profit;
                 }
             }
-            next=curr;
         }
-        return next[1][2];
-    }
-    int maxProfit(vector<int>& prices) {
-        // int n = prices.size();
-        // dp.assign(n + 1, vector<vector<int>>(2, vector<int>(3, 0)));
-        // return solve(0, 1, prices, 2);
-        return tabulation(prices);
+        return dp[0][1][2];
     }
 };
