d549950541184468a331a1db80dd9104172b7d68
Himanshu Sharma <himanshusharma12283@gmail.com>
2025-12-11 01:01:04 +0530

    Time: 7 ms (93.26%) | Memory: 12.7 MB (19.33%) - LeetSync

--- BinarySearchTree/368-largest-divisible-subset/largest-divisible-subset.cpp
+++ BinarySearchTree/368-largest-divisible-subset/largest-divisible-subset.cpp
@@ -1,32 +1,37 @@
 class Solution {
 public:
     vector<int> largestDivisibleSubset(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
         int n = nums.size();
-        int lastIdx = 0, lis = 0;
+        if(n==1) return nums;
+        sort(nums.begin(),nums.end());
         vector<int> dp(n, 1);
         vector<int> parent(n, -1);
+        int ans = 1;
+        int index = 0;
+
         for (int i = 1; i < n; i++) {
             for (int j = 0; j < i; j++) {
-                if (nums[i] % nums[j] == 0) {
-                    if (dp[i] < dp[j] + 1) {
-                        dp[i] = dp[j] + 1;
-                        parent[i] = j;
-                    }
-                    if (lis < dp[i]) {
-                        lis = dp[i];
-                        lastIdx = i;
+                if ((nums[i] % nums[j] == 0) &&
+                    dp[i] < dp[j] + 1) {
+                        
+                    dp[i] = dp[j] + 1;
+                    parent[i] = j;
+
+                    if (ans < dp[i]) {
+                        ans = dp[i];
+                        index = i;
+                        
                     }
                 }
             }
         }
 
-        vector<int>result;
-        while(lastIdx!=-1){
-            result.push_back(nums[lastIdx]);
-            lastIdx=parent[lastIdx];
+        vector<int> result;
+        while (index != -1) {
+            result.push_back(nums[index]);
+            index = parent[index];
         }
-        reverse(result.begin(),result.end());
+
         return result;
     }
 };
