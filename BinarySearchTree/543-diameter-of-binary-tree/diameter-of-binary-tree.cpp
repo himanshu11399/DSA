bbb88da73e9c874d3ebb1e31b51976bfaeef052b
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-01-14 19:42:16 +0530

    Time: 0 ms (100.00%) | Memory: 23.5 MB (92.42%) - LeetSync

--- BinarySearchTree/543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ BinarySearchTree/543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -6,23 +6,25 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-int height(TreeNode*root){
-    if(!root) return 0;
-    int left=height(root->left);
-    int right=height(root->right);
-    return  max(left,right)+1;
-}
+    int maxlen = 0;
+    int height(TreeNode* root) {
+        if (!root)
+            return 0;
+        int left = height(root->left);
+        int right = height(root->right);
+        maxlen = max(maxlen, (left + right));
+        return max(left, right) + 1;
+    }
     int diameterOfBinaryTree(TreeNode* root) {
-        if(!root) return 0;
-        int left=height(root->left);
-        int right=height(root->right);
-        int maxv=max(diameterOfBinaryTree(root->left),diameterOfBinaryTree(root->right));
-        return max((left+right),maxv);
-
+        if (!root)
+            return 0;
+        height(root);
+        return maxlen;
     }
 };
