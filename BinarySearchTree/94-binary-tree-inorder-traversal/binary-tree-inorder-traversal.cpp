d42431b0de1d676bbbe0c74fa795d5e97ab38e2a
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-01-18 20:05:04 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (33.06%) - LeetSync

--- BinarySearchTree/94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ BinarySearchTree/94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -6,17 +6,40 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-vector<int>ans;
     vector<int> inorderTraversal(TreeNode* root) {
-       if(root==NULL) return ans;
-       inorderTraversal(root->left);
-       ans.push_back(root->val);
-       inorderTraversal(root->right);
-       return ans; 
+        vector<int> ans;
+        if (!root)
+            return ans;
+        // Morris Traversal
+        while (root) {
+            // If left is not Exist
+            if (!root->left) {
+                ans.push_back(root->val);
+                root = root->right;
+            } else {
+                // If left is exist;
+                TreeNode* curr = root->left;
+                while (curr->right && curr->right != root) {
+                    curr = curr->right;
+                }
+
+                // If traversed First
+                if (curr->right == root) {
+                    ans.push_back(root->val);
+                    curr->right = NULL;
+                    root = root->right;
+                } else {
+                    curr->right = root;
+                    root = root->left;
+                }
+            }
+        }
+        return ans;
     }
 };
