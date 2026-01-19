ff98ba38601373e28ee2abecddc7af3dcd5714e6
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-01-19 18:31:23 +0530

    Time: 61 ms (60.13%) | Memory: 197.6 MB (27.51%) - LeetSync

--- BinarySearchTree/2567-closest-nodes-queries-in-a-binary-search-tree/closest-nodes-queries-in-a-binary-search-tree.cpp
+++ BinarySearchTree/2567-closest-nodes-queries-in-a-binary-search-tree/closest-nodes-queries-in-a-binary-search-tree.cpp
@@ -6,42 +6,47 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    void inorder(TreeNode* root, set<int>& st) {
-        if (!root)
-            return;
-        inorder(root->left, st);
-        st.insert(root->val);
-        inorder(root->right, st);
+    void inorder(TreeNode*root,vector<int>&arr){
+        if(!root) return;
+        inorder(root->left,arr);
+        arr.push_back(root->val);
+        inorder(root->right,arr);
+    }
+
+    vector<int> search(vector<int>&arr,int target){
+        int floor=-1;
+        int ceil=-1;
+        int low=0;
+        int high=arr.size()-1;
+
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]==target){
+                floor=ceil=arr[mid];
+                break;
+            }else if(arr[mid]>target){
+                ceil=arr[mid];
+                high=mid-1;
+            }else{
+                floor=arr[mid];
+                low=mid+1;
+            }
+        }
+
+        return {floor,ceil};
     }
 
     vector<vector<int>> closestNodes(TreeNode* root, vector<int>& queries) {
-        vector<vector<int>> ans;
-        set<int> st;
-        inorder(root, st);
-
-        for (int i = 0; i < queries.size(); i++) {
-            int floor = -1, ceil = -1;
-            int x=queries[i];
-
-            auto it1 = st.lower_bound(x);
-            if (it1 != st.end()) {
-                ceil = *it1;
-            }
-
-            // Floor
-            auto it2 = st.upper_bound(x);
-            if (it2 != st.begin()) {
-                it2--;
-                floor = *it2;
-            }
-
-            ans.push_back({floor, ceil});
+        vector<vector<int>>ans;
+        vector<int>arr;
+        inorder(root,arr);
+        for(int i=0;i<queries.size();i++){
+            ans.push_back(search(arr,queries[i]));
         }
         return ans;
     }
