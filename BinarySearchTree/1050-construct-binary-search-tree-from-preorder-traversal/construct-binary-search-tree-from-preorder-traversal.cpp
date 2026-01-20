c047dc12a4a21b4e837d504927e438b429cf62ad
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-01-20 18:53:55 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (45.93%) - LeetSync

--- BinarySearchTree/1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
+++ BinarySearchTree/1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
@@ -6,20 +6,40 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    TreeNode*build(vector<int>arr,int& i,int maxval){
-        if(i==arr.size() || arr[i]>maxval) return NULL;
-        TreeNode*root=new TreeNode(arr[i]); i++;
-        root->left=build(arr,i,root->val);
-        root->right=build(arr,i,maxval);
-        return root;
+    int find(vector<int>& inorder, int st, int end, int target) {
+        for (int i = st; i <= end; i++) {
+            if (inorder[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    TreeNode* build(vector<int>& preorder, vector<int>& inorder, int& PreIdx,
+                    int left, int right) {
+        if (left > right)
+            return NULL;
+
+        TreeNode* curr = new TreeNode(preorder[PreIdx]);
+        int InoIdx = find(inorder, left, right, preorder[PreIdx]);
+        PreIdx++;
+        curr->left = build(preorder, inorder, PreIdx, left, InoIdx - 1);
+        curr->right = build(preorder, inorder, PreIdx, InoIdx + 1, right);
+        return curr;
     }
     TreeNode* bstFromPreorder(vector<int>& preorder) {
-        int i=0;
-        return build(preorder,i,INT_MAX);
+        vector<int> inorder;
+        for (int i = 0; i < preorder.size(); i++) {
+            inorder.push_back(preorder[i]);
+        }
+        sort(inorder.begin(), inorder.end());
+
+        int PreIdx = 0;
+        return build(preorder, inorder, PreIdx, 0, inorder.size() - 1);
     }
 };
