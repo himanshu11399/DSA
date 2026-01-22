d5e91e26854f090584ae21d8d9ba629f32ebf13f
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-01-22 22:10:06 +0530

    Time: 1 ms (26.89%) | Memory: 60.8 MB (53.55%) - LeetSync

--- BinarySearchTree/99-recover-binary-search-tree/recover-binary-search-tree.cpp
+++ BinarySearchTree/99-recover-binary-search-tree/recover-binary-search-tree.cpp
@@ -6,32 +6,35 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-   TreeNode*prev=NULL;
-   TreeNode*first=NULL;
-   TreeNode*sec=NULL;
+    TreeNode* prev = NULL;
+    TreeNode* first = NULL;
+    TreeNode* second = NULL;
 
-   void inorder(TreeNode*root){
-    if(root==NULL) return;
-    inorder(root->left);
-    if(prev!=NULL && root->val<prev->val){
-        if(first==NULL){
-            first=prev;
+    void inorder(TreeNode* root) {
+        if (!root)
+            return;
+
+        inorder(root->left);
+
+        // validate
+        if (prev && prev->val > root->val) {
+            if (!first) {
+                first = prev;
+            }
+            second = root;
         }
-        sec=root;
+        prev = root;
+        inorder(root->right);
     }
-    prev=root;
-    inorder(root->right);
-   }
 
     void recoverTree(TreeNode* root) {
-       inorder(root);
-       int temp=first->val;
-       first->val=sec->val;
-       sec->val=temp;
+        inorder(root);
+        swap(first->val, second->val);
     }
 };
