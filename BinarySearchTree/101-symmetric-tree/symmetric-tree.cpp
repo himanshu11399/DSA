4533f6c02262f07f27378b6792626425f4a7cb22
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-02-17 23:31:49 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (26.70%) - LeetSync

--- BinarySearchTree/101-symmetric-tree/symmetric-tree.cpp
+++ BinarySearchTree/101-symmetric-tree/symmetric-tree.cpp
@@ -15,17 +15,17 @@ public:
     bool isSymmetricUntil(TreeNode* t1, TreeNode* t2) {
         if (!t1 && !t2)
             return true;
-        if ((t1 && !t2) || (!t1 && t2))
+        if ((!t1 && t2) || (t1 && !t2))
             return false;
         if (t1->val != t2->val)
             return false;
         return isSymmetricUntil(t1->left, t2->right) &&
                isSymmetricUntil(t1->right, t2->left);
     }
+
     bool isSymmetric(TreeNode* root) {
         if (!root)
             return true;
-
         return isSymmetricUntil(root->left, root->right);
     }
 };
