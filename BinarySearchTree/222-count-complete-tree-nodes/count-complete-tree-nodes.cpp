d152d3c7e9ba580910e5ac07255f683192440cfb
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-02-26 23:27:07 +0530

    Time: 3 ms (4.64%) | Memory: 32.1 MB (7.71%) - LeetSync

--- BinarySearchTree/222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
+++ BinarySearchTree/222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -6,28 +6,27 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     int countNodes(TreeNode* root) {
-        if (!root)
-            return 0;
-        int total = 0;
-        queue<TreeNode*> q;
-        q.push(root);
-
-        while (!q.empty()) {
-            TreeNode* it = q.front();
-            q.pop();
-            total += 1;
-            if (it->left)
-                q.push(it->left);
-            if (it->right)
-                q.push(it->right);
+      if(!root) return 0;
+      int total=0;
+      queue<TreeNode*>q;
+      q.push(root);
+      while(!q.empty()){
+        TreeNode*curr=q.front();
+        q.pop();
+        total+=1;
+        if(curr->left){
+            q.push(curr->left);
         }
-        return total;
+        if(curr->right){
+            q.push(curr->right);
+        }
+      } 
+      return total; 
     }
 };
