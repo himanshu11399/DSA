1699fde1b8bbb6a938b97698cfb3d0a04fb11edc
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-03-05 23:32:21 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (86.15%) - LeetSync

--- BinarySearchTree/70-climbing-stairs/climbing-stairs.cpp
+++ BinarySearchTree/70-climbing-stairs/climbing-stairs.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
-    vector<int>dp;
-    int solve(int n) {
-        if (n == 0) {
-            return 1;
-        }
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        int ans=0;
-        //take 1
-        ans+=solve(n - 1);
-        //take 2
-        if (n >= 2) {
-            ans+=solve(n - 2);
-        }
-        return dp[n]=ans;
-    }
+    // vector<int> dp;
+    // int solve(int n) {
+    //     if (n == 0 || n == 1)
+    //         return 1;
+    //     if (n < 0)
+    //         return 0;
+    //     if (dp[n] != 0)
+    //         return dp[n];
+    //     int one = climbStairs(n - 1);
+    //     int two = climbStairs(n - 2);
+
+    //     return dp[n]=one + two;
+    // }
 
     int climbStairs(int n) {
-        dp.assign(n+1,-1);
-        return solve(n);
+        if (n <= 1)
+            return 1;
+        
+        if(n<=1) return 1;
+       int a=1,b=1;
+       for(int i=2;i<=n;i++){
+          int c=a+b;
+          a=b;
+          b=c;
+       } 
+       return b;
     }
 };
