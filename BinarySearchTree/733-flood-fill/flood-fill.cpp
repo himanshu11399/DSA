fec5664855eb5dc03b7a06c8805338d61c029219
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-03-12 23:19:23 +0530

    Time: 2 ms (17.19%) | Memory: 19.2 MB (8.84%) - LeetSync

--- BinarySearchTree/733-flood-fill/flood-fill.cpp
+++ BinarySearchTree/733-flood-fill/flood-fill.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
-    vector<vector<int>> dir = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
     vector<vector<int>> floodFill(vector<vector<int>>& grid, int sr, int sc,
                                   int color) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>> ans(n, vector<int>(m, -1));
+        int pixel = grid[sr][sc];
+
         vector<vector<int>> vis(n, vector<int>(m, 0));
+        vector<vector<int>> dir = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
         queue<pair<int, int>> q;
         q.push({sr, sc});
-        ans[sr][sc] = color;
+        vis[sr][sc] = 1;
+        grid[sr][sc] = color;
 
         while (!q.empty()) {
             int sz = q.size();
@@ -17,29 +19,18 @@ public:
             for (int i = 0; i < sz; i++) {
                 auto [row, col] = q.front();
                 q.pop();
-                for (auto& it : dir) {
-                    int nrow = row + it[0];
-                    int ncol = col + it[1];
+                for (auto it : dir) {
+                    int nrow = it[0] + row;
+                    int ncol = it[1] + col;
                     if (nrow >= 0 && ncol >= 0 && nrow < n && ncol < m &&
-                        vis[nrow][ncol] == 0 &&
-                        grid[row][col] == grid[nrow][ncol]) {
+                        grid[nrow][ncol] == pixel && vis[nrow][ncol] == 0) {
                         q.push({nrow, ncol});
-                        vis[nrow][ncol]=1;
-                        ans[nrow][ncol] = color;
+                        vis[nrow][ncol] = 1;
+                        grid[nrow][ncol] = color;
                     }
                 }
             }
         }
-
-        // copy the remaining colors
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (ans[i][j] != color) {
-                    ans[i][j] = grid[i][j];
-                }
-            }
-        }
-
-        return ans;
+        return grid;
     }
 };
