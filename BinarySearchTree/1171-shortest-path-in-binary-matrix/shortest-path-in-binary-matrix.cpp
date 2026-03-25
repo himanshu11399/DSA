0945b5854f8856982238a9ad859bc0eefac9df34
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-03-26 03:15:10 +0530

    Time: 263 ms (5.20%) | Memory: 84.7 MB (5.23%) - LeetSync

--- BinarySearchTree/1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ BinarySearchTree/1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,46 +1,43 @@
 class Solution {
 public:
-    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int n = grid.size();
-
-        if (grid[0][0] == 1 || grid[n - 1][n - 1]) {
+    int shortestPathBinaryMatrix(vector<vector<int>>& nums) {
+        int n = nums.size();
+        if (nums[0][0] == 1 || nums[n - 1][n - 1] == 1) {
             return -1;
         }
 
-        if(n==1) return 1;
+        if (n == 1) {
+            return 1;
+        }
 
-        vector<vector<int>> dis(n, vector<int>(n, 1e9));
-        vector<vector<int>> dir = {{0, 1}, {1, 0},   {0, -1}, {-1, 0},
-                                   {1, 1}, {-1, -1}, {-1, 1}, {1, -1}};
+        vector<vector<int>>vis(n, vector<int>(n,1e9));
+        vector<vector<int>> dir = {{0, 1},  {1, 0},  {-1, 0},
+                                   {0, -1}, {-1, 1}, {1, -1},{-1,-1},{1,1}};
 
-        queue<pair<int, pair<int, int>>> q;
-        q.push({1, {0, 0}});
+        queue<pair<int, int>> q;
+        q.push({0, 0});
+        vis[0][0] = 1;
 
         while (!q.empty()) {
-            auto it = q.front();
+            auto [row, col] = q.front();
             q.pop();
 
-            int wt = it.first;
-            int u = it.second.first;
-            int v = it.second.second;
-
             for (auto it : dir) {
-                int nrow = u + it[0];
-                int ncol = v + it[1];
+                int nrow = row + it[0];
+                int ncol = col + it[1];
 
-                if (nrow >= 0 && ncol >= 0 && nrow < n && ncol < n &&
-                    grid[nrow][ncol] == 0) {
-
-                    if (dis[nrow][ncol] > wt + 1) {
-                        dis[nrow][ncol] = min(dis[nrow][ncol],wt + 1);
-                        q.push({wt + 1, {nrow, ncol}});
+                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < n &&
+                    nums[nrow][ncol] == 0) {
+                    if (vis[nrow][ncol] > vis[row][col] + 1) {
+                        vis[nrow][ncol] = vis[row][col] + 1;
+                        q.push({nrow, ncol});
                     }
                 }
             }
         }
-        if (dis[n - 1][n - 1] == 1e9) {
+        if (vis[n - 1][n - 1] == 1e9) {
             return -1;
         }
-        return dis[n - 1][n - 1];
+        return vis[n - 1][n - 1];
     }
 };
