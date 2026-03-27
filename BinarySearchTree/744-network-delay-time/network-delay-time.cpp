cc72e79304bc5bf21fa17fa14c6d9551d1997bf7
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-03-27 10:01:41 +0530

    Time: 94 ms (26.66%) | Memory: 48 MB (18.84%) - LeetSync

--- BinarySearchTree/744-network-delay-time/network-delay-time.cpp
+++ BinarySearchTree/744-network-delay-time/network-delay-time.cpp
@@ -1,39 +1,42 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int src) {
-        vector<vector<pair<int, int>>> adj(n + 1);
-        //{v,wt}
+        vector < vector<pair<int, int>>> adj(n+1);
         for (auto it : times) {
             adj[it[0]].push_back({it[1], it[2]});
         }
 
         priority_queue<pair<int, int>, vector<pair<int, int>>,
-                       greater<pair<int, int>>>pq;
+                       greater<pair<int, int>>>
+            pq;
+        vector<int> dist(n+1,1e9);
+
         pq.push({0, src});
-        //{dis,src}
-        vector<int> dist(n + 1, 1e9);
         dist[src] = 0;
+
         while (!pq.empty()) {
             auto it = pq.top();
-            int cost = it.first;
-            int node = it.second;
             pq.pop();
+
+            int wt = it.first;
+            int node = it.second;
+
             for (auto i : adj[node]) {
-                int adjnode = i.first;
-                int wt = i.second;
-                if (cost + wt < dist[adjnode]) {
-                    pq.push({cost + wt, adjnode});
-                    dist[adjnode] = cost + wt;
+                int adjNode = i.first;
+                int dis = i.second;
+
+                if (dist[adjNode] > wt + dis) {
+                    pq.push({wt + dis, adjNode});
+                    dist[adjNode] = wt + dis;
                 }
             }
         }
-        int maxdelay = 0;
-        for (int i = 1; i <= n; i++) {
-            if (dist[i] == 1e9) {
-                return -1;
-            }
-            maxdelay = max(maxdelay, dist[i]);
+
+        int maxans = INT_MIN;
+        for (int i = 1; i < n+1; i++) {
+            maxans = max(maxans, dist[i]);
         }
-        return maxdelay;
+
+        return maxans==1e9?-1:maxans;
     }
 };
