cee30e0707a43fdd70377b23f0480d4a643c9ae7
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-04-03 09:56:08 +0530

    Time: 55 ms (64.11%) | Memory: 14.3 MB (58.02%) - LeetSync

--- BinarySearchTree/300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ BinarySearchTree/300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -3,18 +3,17 @@ public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
         vector<int> ans(n, 1);
-        int maxlen = 1;
+        int maxans = 1;
         for (int i = 1; i < n; i++) {
-            for (int j = i-1; j >= 0; j--) {
+            for (int j = i - 1; j >= 0; j--) {
                 if (nums[i] > nums[j]) {
-                    ans[i] = max(ans[i], ans[j]+1);
+                    ans[i] = max(ans[i], ans[j] + 1);
                 }
             }
         }
-
         for (int i = 0; i < n; i++) {
-            maxlen = max(maxlen, ans[i]);
+            maxans = max(maxans, ans[i]);
         }
-        return maxlen;
+        return maxans;
     }
 };
