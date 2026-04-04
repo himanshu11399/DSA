b2002d3b8982c74d9d74c785edf433a203a48e11
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-04-04 15:38:10 +0530

    Time: 7 ms (6.49%) | Memory: 18.7 MB (5.04%) - LeetSync

--- BinarySearchTree/1036-rotting-oranges/rotting-oranges.cpp
+++ BinarySearchTree/1036-rotting-oranges/rotting-oranges.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
-    vector<vector<int>> dir = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
     int orangesRotting(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        int count = 0;
 
         vector<vector<int>> vis(n, vector<int>(m, 0));
-
         queue<pair<int, int>> q;
 
-        // Find the rooten orange
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (grid[i][j] == 2) {
@@ -20,39 +16,41 @@ public:
             }
         }
 
-        // Apply the BFS
-        while (!q.empty()) {
-            int sz = q.size();
-            bool isroot = false;
+        vector<vector<int>> dir = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+        int count = 0;
 
-            for (int i = 0; i < sz; i++) {
+        while (!q.empty()) {
+            int lvl = q.size();
+            int isroot=false;
+
+            for (int i = 0; i < lvl; i++) {
                 auto [row, col] = q.front();
                 q.pop();
-                for (auto& it : dir) {
+
+                for (auto it : dir) {
                     int nrow = row + it[0];
                     int ncol = col + it[1];
                     if (nrow >= 0 && ncol >= 0 && nrow < n && ncol < m &&
-                        grid[nrow][ncol] == 1 && vis[nrow][ncol] == 0) {
-                        q.push({nrow, ncol});
+                        vis[nrow][ncol] == 0 && grid[nrow][ncol] == 1) {
                         vis[nrow][ncol] = 1;
-                        grid[nrow][ncol] = 2;
-                        isroot = true;
+                        grid[nrow][ncol]=2;
+                        q.push({nrow, ncol});
+                        isroot=true;
                     }
                 }
             }
-            if (isroot)
+            if(isroot){
                 count++;
-        }
-
-        // Check if any fresh orange is remain
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 1)
-                    return -1;
             }
         }
 
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1) {
+                    return -1;
+                }
+            }
+        }
         return count;
     }
 };
