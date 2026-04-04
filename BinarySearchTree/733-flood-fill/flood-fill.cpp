b84a721a344fe264f8616092509bc828e4a43db8
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-04-04 15:58:17 +0530

    Time: 3 ms (13.56%) | Memory: 19.2 MB (5.05%) - LeetSync

--- BinarySearchTree/733-flood-fill/flood-fill.cpp
+++ BinarySearchTree/733-flood-fill/flood-fill.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& grid, int sr, int sc,
+    vector<vector<int>> floodFill(vector<vector<int>>& nums, int sr, int sc,
                                   int color) {
-        int n = grid.size();
-        int m = grid[0].size();
-        int pixel = grid[sr][sc];
+        int n = nums.size();
+        int m = nums[0].size();
 
         vector<vector<int>> vis(n, vector<int>(m, 0));
-        vector<vector<int>> dir = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
+        vector<vector<int>> dir = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
         queue<pair<int, int>> q;
         q.push({sr, sc});
+        int originalColor=nums[sr][sc];
+        nums[sr][sc] = color;
         vis[sr][sc] = 1;
-        grid[sr][sc] = color;
 
         while (!q.empty()) {
             int sz = q.size();
@@ -19,18 +19,19 @@ public:
             for (int i = 0; i < sz; i++) {
                 auto [row, col] = q.front();
                 q.pop();
+
                 for (auto it : dir) {
-                    int nrow = it[0] + row;
-                    int ncol = it[1] + col;
+                    int nrow = row + it[0];
+                    int ncol = col + it[1];
                     if (nrow >= 0 && ncol >= 0 && nrow < n && ncol < m &&
-                        grid[nrow][ncol] == pixel && vis[nrow][ncol] == 0) {
-                        q.push({nrow, ncol});
+                        vis[nrow][ncol] == 0 && nums[nrow][ncol] == originalColor) {
+                        nums[nrow][ncol] = color;
                         vis[nrow][ncol] = 1;
-                        grid[nrow][ncol] = color;
+                        q.push({nrow,ncol});
                     }
                 }
             }
         }
-        return grid;
+        return nums;
     }
 };
