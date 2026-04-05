9d9d581532e5d64980a220a33ada67176e789d8d
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-04-06 01:57:09 +0530

    Time: 42 ms (25.40%) | Memory: 27.7 MB (6.10%) - LeetSync

--- BinarySearchTree/1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ BinarySearchTree/1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,35 +1,32 @@
 class Solution {
 public:
-    // vector<vector<int>>dp;
-    // int solve(int i,int j,string& t1,string& t2){
-    //     if(i>=t1.size() || j>=t2.size()) return 0;
-    //     if(dp[i][j]!=-1) return dp[i][j];
+    vector<vector<int>> dp;
+    int solve(string& s1, string& s2, int i, int j) {
+        if (i >= s1.size() || j >= s2.size()) {
+            return 0;
+        }
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        int ispos = 0;
+        int row = 0;
+        int col = 0;
+        if (s1[i] == s2[j]) {
+            ispos = solve(s1, s2, i + 1, j + 1) + 1;
+        } else {
+            // Increse i
+            row = solve(s1, s2, i + 1, j);
 
-    //     int take=0;
-    //     if(t1[i]==t2[j]){
-    //       take=1+solve(i+1,j+1,t1,t2);
-    //     }
-    //     int notake=max(solve(i+1,j,t1,t2),solve(i,j+1,t1,t2));
+            // Increase J
+            col = solve(s1, s2, i, j + 1);
+        }
+        return dp[i][j] = max(ispos, max(row, col));
+    }
 
-    //     return dp[i][j]=max(take,notake);
-
-    // }
     int longestCommonSubsequence(string text1, string text2) {
         int n = text1.size();
         int m = text2.size();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
-
-        for (int i = 1; i <= n; i++) {
-
-            for (int j = 1; j <= m; j++) {
-
-                if (text1[i - 1] == text2[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                }
-
-                dp[i][j] = max(dp[i][j], max(dp[i - 1][j], dp[i][j - 1]));
-            }
-        }
-        return dp[n][m];
+        dp.assign(n, vector<int>(m, -1));
+        return solve(text1, text2, 0, 0);
     }
 };
