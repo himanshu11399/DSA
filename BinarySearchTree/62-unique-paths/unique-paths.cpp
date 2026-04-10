8ec7a8ce1fcdf86e3ea2ace762f82045a79cf9e9
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-04-10 12:34:13 +0530

    Time: 3 ms (9.25%) | Memory: 9.3 MB (50.25%) - LeetSync

--- BinarySearchTree/62-unique-paths/unique-paths.cpp
+++ BinarySearchTree/62-unique-paths/unique-paths.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int solve(int i, int j, int& m, int& n) {
-        if (i > m - 1 || j > n - 1)
-            return 0;
-        if (i == m - 1 && j == n - 1)
-            return 1;
-        if (dp[i][j] != -1)
-            return dp[i][j];
-
-        // move down
-        int down = solve(i + 1, j, m, n);
-
-        // move right
-        int right = solve(i, j + 1, m, n);
-
-        return dp[i][j] = down + right;
+vector<vector<int>>dp;
+int solve(int& m,int& n,int i,int j){
+    if(i==m-1 && j==n-1){
+        return 1;
+    }
+    if(i>=m || j>=n){
+        return 0;
+    }
+    if(dp[i][j]!=-1){
+        return dp[i][j];
     }
 
+    //option1;
+    int right=solve(m,n,i,j+1);
+
+    //Option2:-
+    int bottom=solve(m,n,i+1,j);
+
+    return dp[i][j]=right+bottom;
+}
     int uniquePaths(int m, int n) {
-        dp.assign(m + 1, vector<int>(n + 1, -1));
-        return solve(0, 0, m, n);
+       dp.assign(m,vector<int>(n,-1));
+       return solve(m,n,0,0); 
     }
 };
