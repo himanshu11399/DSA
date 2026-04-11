6182850fad95fa20cd4aec7870753aedec559746
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-04-11 16:48:35 +0530

    Time: 2 ms (34.35%) | Memory: 16.4 MB (39.34%) - LeetSync

--- BinarySearchTree/64-minimum-path-sum/minimum-path-sum.cpp
+++ BinarySearchTree/64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int solve(vector<vector<int>>& nums, int row, int col) {
-        if (row >= nums.size() || col >= nums[0].size()) {
-            return 1e9;
-        }
-        if (row == nums.size() - 1 && col == nums[0].size() - 1) {
-            return nums[row][col];
-        }
-        if (dp[row][col] != -1) {
-            return dp[row][col];
-        }
-
-        int right = solve(nums, row, col + 1);
-        int bottom = solve(nums, row + 1, col);
-
-        return dp[row][col] = min(right, bottom)+nums[row][col];
-    }
     int minPathSum(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        dp.assign(n, vector<int>(m, -1));
-        return solve(grid, 0, 0);
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 1e9));
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(i==1 && j==1){
+                    dp[i][j]=grid[i-1][j-1];
+                    continue;
+                }
+                int right=dp[i][j-1];
+                int bottom=dp[i-1][j];
+                dp[i][j]=min(right,bottom)+grid[i-1][j-1];
+            }
+        }
+        return dp[n][m];
     }
 };
