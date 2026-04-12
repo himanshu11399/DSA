d3856159943389aae59de109c83530222972f2ec
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-04-12 18:18:10 +0530

    Time: 123 ms (59.64%) | Memory: 111.4 MB (14.59%) - LeetSync

--- BinarySearchTree/416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ BinarySearchTree/416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,37 +1,33 @@
 class Solution {
 public:
-    bool isSubsetSum(vector<int>& arr, int target) {
-        int n = arr.size();
-
-        vector<vector<bool>> dp(n + 1, vector<bool>(target + 1, false));
-
+    bool findtarget(vector<int>& nums, int& target) {
+        int n = nums.size();
+        vector<vector<int>> dp(n + 1, vector<int>(target + 1, 0));
         for (int i = 0; i <= n; i++) {
-            dp[i][0] = true;
+            dp[i][0] = 1;
         }
-
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= target; j++) {
-                // Exclude
-                dp[i][j] = dp[i - 1][j];
-                
-                if (arr[i - 1] <= j) {
-                    dp[i][j] = dp[i][j] || dp[i - 1][j - arr[i - 1]];
+                if (nums[i - 1] > j) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = (dp[i - 1][j] || dp[i - 1][j - nums[i - 1]]);
                 }
             }
         }
-        return dp[n - 1][target];
+        return dp[n][target];
     }
-
     bool canPartition(vector<int>& nums) {
+        int n = nums.size();
         int total = 0;
-        for (int i = 0; i < nums.size(); i++) {
+        for (int i = 0; i < n; i++) {
             total += nums[i];
         }
 
-        if (total % 2 != 0)
+        if (total % 2 != 0) {
             return false;
-
-        int find = total / 2;
-        return isSubsetSum(nums, find);
+        }
+        int target = total / 2;
+        return findtarget(nums, target);
     }
 };
