7c7373e6f619786d832bd63704f5503beb720137
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-04-16 13:40:30 +0530

    Time: 7 ms (71.25%) | Memory: 44.7 MB (97.20%) - LeetSync

--- BinarySearchTree/455-assign-cookies/assign-cookies.cpp
+++ BinarySearchTree/455-assign-cookies/assign-cookies.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        // step-1 to sort the both vector
-        sort(g.begin(), g.end());
-        sort(s.begin(), s.end());
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
 
-        int i = 0, j = 0;
-        while (g.size() > i && s.size() > j) {
-            if (s[j] >= g[i]) {
+        int i=0;
+        int j=0;
+        int ans=0;
+        while(i<g.size() && j<s.size()){
+            if(g[i]>s[j]){
+                j++;
+            }else{
+                ans++;
                 i++;
+                j++;
             }
-            j++;
         }
-        return i;
+        return ans;
     }
 };
