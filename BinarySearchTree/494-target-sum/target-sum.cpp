352e7ba74a90b16f0f8df483018bb392517bc488
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-04-17 23:36:36 +0530

    Time: 370 ms (26.37%) | Memory: 188 MB (5.01%) - LeetSync

--- BinarySearchTree/494-target-sum/target-sum.cpp
+++ BinarySearchTree/494-target-sum/target-sum.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
-    unordered_map<string,int>mpp;
+    // unordered_map<string,int>mpp;
+    int sum = 0;
+    vector<vector<int>>dp;
     int solve(vector<int> nums, int i, int currsum, int target) {
         if (i == nums.size()) {
             if (target == currsum) {
@@ -9,17 +11,27 @@ public:
                 return 0;
             }
         }
-        string key=to_string(i)+"_"+to_string(currsum);
-        if(mpp.count(key)){
-            return mpp[key];
+        // string key=to_string(i)+"_"+to_string(currsum);
+        // if(mpp.count(key)){
+        //     return mpp[key];
+        // }
+
+        if(dp[i][currsum+sum]!=-1){
+            return dp[i][currsum+sum];
         }
 
         int add = solve(nums, i + 1, currsum + nums[i], target);
         int sub = solve(nums, i + 1, currsum - nums[i], target);
 
-        return mpp[key]=add + sub;
+        // return mpp[key]=add + sub;
+        return dp[i][currsum + sum] = add + sub;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+        }
+        dp.assign(n + 1, vector<int>(2 * sum+1, -1));
         return solve(nums, 0, 0, target);
     }
 };
