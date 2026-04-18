7ed8732650c43ace9155ae9df39d4dc5d6e048a8
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-04-18 15:18:08 +0530

    Time: 39 ms (28.46%) | Memory: 27.6 MB (14.30%) - LeetSync

--- BinarySearchTree/1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ BinarySearchTree/1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,35 +1,26 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int solve(string& s1, string& s2, int i, int j) {
-        if (i >= s1.size() || j >= s2.size()) {
-            return 0;
-        }
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
+vector<vector<int>>dp;
+    int solve(string&s1,string&s2,int i,int j){
+     if(i>=s1.size() || j>=s2.size()){
+        return 0;
+     }
+     if(dp[i][j]!=-1){
+        return dp[i][j];
+     }
+     if(s1[i]==s2[j]){
+        return solve(s1,s2,i+1,j+1)+1;
+     }
 
-        // Match
-        int isMatch = 0;
-        int left = 0;
-        int right = 0;
-        if (s1[i] == s2[j]) {
-            isMatch = solve(s1, s2, i + 1, j + 1) + 1;
-        } else {
+     int left=solve(s1,s2,i+1,j);
+     int right=solve(s1,s2,i,j+1);
 
-            // increse i
-            left = solve(s1, s2, i + 1, j);
-
-            // Increase j
-            right = solve(s1, s2, i, j + 1);
-        }
-
-        return dp[i][j] = max(isMatch, max(left, right));
+     return dp[i][j]=max(left,right);
     }
     int longestCommonSubsequence(string text1, string text2) {
-        int n = text1.size();
-        int m = text2.size();
-        dp.assign(n, vector<int>(m, -1));
-        return solve(text1, text2, 0, 0);
+      int n=text1.size();
+      int m=text2.size();
+      dp.assign(n+1,vector<int>(m+1,-1));
+      return solve(text1,text2,0,0);  
     }
 };
