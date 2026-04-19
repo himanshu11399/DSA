668f52c01ac43fd42e434d301b2d9093bc08328f
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-04-19 21:50:08 +0530

    Time: 34 ms (36.90%) | Memory: 27 MB (62.93%) - LeetSync

--- BinarySearchTree/115-distinct-subsequences/distinct-subsequences.cpp
+++ BinarySearchTree/115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
+    vector<vector<int>> dp;
+    int solve(string& str1, string& str2, int i, int j) {
+        if (i >= str1.size()) {
+            return 1;
+        }
+        if (j >= str2.size())
+            return 0;
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        if (str1[i] == str2[j]) {
+            int take = solve(str1, str2, i + 1, j + 1);
+            int notake = solve(str1, str2, i, j + 1);
+            return dp[i][j] = take+notake;
+        }
+        return dp[i][j] = solve(str1, str2, i, j + 1);
+    }
     int numDistinct(string s, string t) {
-        int n=s.size();
-        int m=t.size();
-        vector<vector<unsigned long long>>dp(n+1,vector<unsigned long long>(m+1,0));
-        for(int i=0;i<=n;i++){
-            dp[i][0]=1;
-        }
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(s[i-1]==t[j-1]){
-                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
-                }else{
-                    dp[i][j]=dp[i-1][j];
-                }
-            }
-        }
-        return (int)dp[n][m];
+        int n = s.size();
+        int m = t.size();
+        dp.assign(m + 1, vector<int>(n + 1, -1));
+        return solve(t, s, 0, 0);
     }
 };
