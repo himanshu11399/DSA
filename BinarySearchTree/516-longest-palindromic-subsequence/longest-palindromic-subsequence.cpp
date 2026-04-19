fd02c869fdd21a0de5c3585c89a92fa22dc2856d
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-04-19 16:24:34 +0530

    Time: 51 ms (78.53%) | Memory: 76.1 MB (21.06%) - LeetSync

--- BinarySearchTree/516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ BinarySearchTree/516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int solve(string& str, int i, int j) {
-        if (i >= str.size() || j < 0 || i > j) {
-            return 0;
-        }
-        if (i == j) {
-            return 1;
-        }
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-
-        int notake = 0;
-        if (str[i] == str[j]) {
-            return dp[i][j] = solve(str, i + 1, j - 1) + 2;
-        }
-        int left = solve(str, i + 1, j);
-        int right = solve(str, i, j-1);
-        return dp[i][j] = max(left, right);
+vector<vector<int>>dp;
+int solve(string& str,int i,int j){
+    if(i>=str.size() || j<0 || i>j){
+        return 0;
     }
+    if(i==j){
+        return 1;
+    }
+    if(dp[i][j]!=-1){
+        return dp[i][j];
+    }
+    if(str[i]==str[j]){
+       return dp[i][j]=solve(str,i+1,j-1)+2;
+    }
+    int left=solve(str,i+1,j);
+    int right=solve(str,i,j-1);
+    return dp[i][j]=max(left,right);
+}
     int longestPalindromeSubseq(string str) {
-        int n = str.size();
-        dp.assign(n + 2, vector<int>(n + 2, -1));
-        return solve(str, 0, n - 1);
+       int n=str.size();
+       dp.assign(n+1,vector<int>(n+1,-1));
+       return solve(str,0,n-1); 
     }
 };
