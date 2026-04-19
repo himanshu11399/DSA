1b6f5db0e6119b753a710937d49f8015e8f1e546
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-04-19 17:01:15 +0530

    Time: 7 ms (88.06%) | Memory: 16.6 MB (70.62%) - LeetSync

--- BinarySearchTree/583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ BinarySearchTree/583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -4,19 +4,18 @@ public:
         int n = word1.size();
         int m = word2.size();
         vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
-        
-        
+
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
                 if (word1[i - 1] == word2[j - 1]) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
-                    
                 } else {
-                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
-    int LCS=dp[n][m];
-    return n+m-2*LCS;
+        int LCS=dp[n][m];
+        int ans=(n-LCS)+(m-LCS);
+        return ans;
     }
 };
