61723304d6c46931ff87322c28f23c7c9335b2a1
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-04-21 00:28:09 +0530

    Time: 6 ms (70.17%) | Memory: 13.3 MB (40.36%) - LeetSync

--- BinarySearchTree/72-edit-distance/edit-distance.cpp
+++ BinarySearchTree/72-edit-distance/edit-distance.cpp
@@ -1,26 +1,46 @@
 class Solution {
 public:
     vector<vector<int>> dp;
-    int solve(int i, int j, string& s1, string& s2, int n, int m) {
-        if (i == n)
-            return m - j;
-        if (j == m)
-            return n - i;
-        if (dp[i][j] != -1)
-            return dp[i][j];
-        if (s1[i] == s2[j]) {
-            return dp[i][j] = solve(i + 1, j + 1, s1, s2, n, m);
+    int solve(string& s1, string& s2, int i, int j) {
+        if (i == s1.size() && j == s2.size()) {
+            return 0;
         }
-        // insert
-        int insert = 1 + solve(i, j + 1, s1, s2, n, m);
-        int del = 1 + solve(i + 1, j, s1, s2, n, m);
-        int update = 1 + solve(i + 1, j + 1, s1, s2, n, m);
-        return dp[i][j] = min({insert, del, update});
+        if (i > s1.size() && j < s2.size()) {
+            return INT_MAX;
+        }
+
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        if (s1[i] == s2[j]) {
+            return dp[i][j] = solve(s1, s2, i + 1, j + 1);
+        }
+
+        int insert = INT_MAX;
+        int update = INT_MAX;
+        int del = INT_MAX;
+
+        // Insert
+        if (j < s2.size()) {
+            insert = solve(s1, s2, i, j + 1) + 1;
+        }
+
+        // Update
+        if (i < s1.size() && j < s2.size()) {
+            update = solve(s1, s2, i + 1, j + 1) + 1;
+        }
+
+        // Delete
+        if (i < s1.size()) {
+            del = solve(s1, s2, i + 1, j) + 1;
+        }
+
+        return dp[i][j] = min(insert, min(update, del));
     }
     int minDistance(string word1, string word2) {
         int n = word1.size();
         int m = word2.size();
         dp.assign(n + 1, vector<int>(m + 1, -1));
-        return solve(0,0, word1, word2, n, m);
+        return solve(word1, word2, 0, 0);
     }
 };
