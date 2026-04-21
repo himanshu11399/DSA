ef8b36a30300110a4220b579c634920330d250fd
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-04-22 03:04:14 +0530

    Time: 131 ms (11.33%) | Memory: 54.8 MB (11.39%) - LeetSync

--- BinarySearchTree/44-wildcard-matching/wildcard-matching.cpp
+++ BinarySearchTree/44-wildcard-matching/wildcard-matching.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
     vector<vector<int>> dp;
-    bool solve(int i, int j, string& s1, string& s2) {
-        if(i<0 && j<0) return true;
-        if(i>=0 && j<0) return false;
-        if(i<0 && j>=0){
-            for(int k=0;k<=j;k++){
-                if(s2[k]!='*'){
-                    return false;
-                }
-            }
-            return true;
+    bool solve(string& str1, string& str2, int i, int j) {
+        if (i == str1.size() && j == str2.size()) {
+            return 1;
+        }
+        if(i==str1.size()){
+            while(j<str2.size()){
+                if(str2[j]!='*') return false;
+                j++;
+            } return true;
+        }
+        if(j>=str2.size()) return 0;
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        if (str1[i] == str2[j] || str2[j] == '?') {
+            return dp[i][j] = solve(str1, str2, i + 1, j + 1);
         }
 
-        if(dp[i][j]!=-1) return dp[i][j];
-
-        if(s1[i]==s2[j] || s2[j]=='?'){
-            return dp[i][j]= solve(i-1,j-1,s1,s2);
-        }else if(s2[j]=='*'){
-            return dp[i][j]= (solve(i-1,j,s1,s2) || solve(i,j-1,s1,s2));
-        }else{
-            return dp[i][j]= false;
+        if (str2[j] == '*') {
+            int take = solve(str1, str2, i + 1, j);
+            int notake = solve(str1, str2, i, j + 1);
+            return dp[i][j] = take || notake;
         }
-
-
+        return dp[i][j] = 0;
     }
     bool isMatch(string s, string p) {
         int n = s.size();
         int m = p.size();
         dp.assign(n + 1, vector<int>(m + 1, -1));
-        return solve(n - 1, m - 1, s, p);
+        return solve(s, p, 0, 0);
     }
 };
