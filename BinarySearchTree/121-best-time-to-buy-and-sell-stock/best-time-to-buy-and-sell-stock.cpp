4b9dea8b39f16b71eae7a4c0e130bf99a71fb542
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-05-02 02:15:39 +0530

    Time: 2 ms (31.16%) | Memory: 97.4 MB (24.18%) - LeetSync

--- BinarySearchTree/121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ BinarySearchTree/121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,32 +1,16 @@
 class Solution {
 public:
-  vector<vector<int>>dp;
-//   int solve(vector<int>&prices,int i,int j){
-//     if(j>=prices.size()){
-//         return 0;
-//     }
-//     if(dp[i][j]!=-1){
-//         return dp[i][j];
-//     }
-
-//     if(prices[i]>prices[j]){
-//     return dp[i][j]=solve(prices,i+1,j+1);
-//     }
-//     int ans=prices[j]-prices[i];
-//     return dp[i][j]=max(ans,solve(prices,i,j+1));
-//   }
-  
     int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        int maxans=INT_MIN;
-        int j=0;
-        for(int i=1;i<n;i++){
-            if(prices[j]>prices[i]){
-              j=i;  
+        int n = prices.size();
+        int maxans = 0;
+        int i = 0;
+        for (int j = 1; j < n; j++) {
+            if (prices[i] > prices[j]) {
+                i=j;
             }else{
-                maxans=max(maxans,prices[i]-prices[j]);
+                maxans=max(maxans,prices[j]-prices[i]);
             }
         }
-        return maxans==INT_MIN?0:maxans;
+        return maxans;
     }
 };
