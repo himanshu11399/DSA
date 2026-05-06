40cdd6b3bf5d69eec567da4a62ff8c70a6a77a10
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-05-07 00:52:57 +0530

    Time: 19 ms (95.29%) | Memory: 17.5 MB (35.49%) - LeetSync

--- BinarySearchTree/673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
+++ BinarySearchTree/673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
     int findNumberOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        if(n==0){
+            return 0;
+        }
+        vector<int>dp(n,1);
+        vector<int>count(n,1);
+        int maxans=0;
 
-        int n = nums.size();
-        if(n==0) return 0;
-        vector<int> dp(n, 1);
-        vector<int> count(n, 1);
-        int lis = 0;
-
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-
-                if (nums[i] > nums[j]) {
-                    if (dp[j] + 1 == dp[i]) {
-                        count[i] += count[j];
-                    } else if (dp[j] + 1 > dp[i]) {
-                        count[i] = count[j];
-                        dp[i] = dp[j] + 1;
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j]){
+                    if(dp[i]==dp[j]+1){
+                        count[i]+=count[j];
+                    }else if(dp[i]<dp[j]+1){
+                        dp[i]=dp[j]+1;
+                        count[i]=count[j];
                     }
                 }
             }
-            lis = max(lis, dp[i]);
+            maxans=max(maxans,dp[i]);
         }
 
-        int result = 0;
-        for (int i = 0; i < n; i++) {
-            if (dp[i] == lis) {
-                result += count[i];
+        int result=0;
+        for(int i=0;i<n;i++){
+            if(dp[i]==maxans){
+                result+=count[i];
             }
         }
-
         return result==0?1:result;
     }
 };
