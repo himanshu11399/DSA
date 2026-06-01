c25cc93cdfcc30011ec586313b32ae4a724dfd52
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-06-02 00:00:01 +0530

    Time: 0 ms (100.00%) | Memory: 22.8 MB (9.20%) - LeetSync

--- BinarySearchTree/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ BinarySearchTree/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int k=0;
-        int prev=nums[0];
-        for(int i=0;i<nums.size();i++){
-           if(i==0 || nums[i]!=prev){
+        int k=1;
+        for(int i=1;i<nums.size();i++){
+           if(nums[i]!=nums[i-1]){
             nums[k]=nums[i];
-            prev=nums[i];
             k++;
            }
         }
