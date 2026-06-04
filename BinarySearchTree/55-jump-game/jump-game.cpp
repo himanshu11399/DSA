9e198927485477175c0058e2922987a314fadb7e
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-06-04 10:15:07 +0530

    Time: 611 ms (6.68%) | Memory: 55.8 MB (6.10%) - LeetSync

--- BinarySearchTree/55-jump-game/jump-game.cpp
+++ BinarySearchTree/55-jump-game/jump-game.cpp
@@ -1,13 +1,28 @@
 class Solution {
 public:
+    vector<int> dp;
+    bool solve(vector<int>& nums, int idx) {
+        if (idx == nums.size() - 1) {
+            return true;
+        }
+        if (dp[idx] != -1) {
+            return dp[idx];
+        }
+        if(nums[idx]==0){
+            return false;
+        }
+        bool take;
+        for (int i = 1; i <= nums[idx]; i++) {
+            // take
+            take = solve(nums, idx + i);
+            if (take==1) {
+                return dp[idx] = 1;
+            }
+        }
+        return dp[idx] = 0;
+    }
     bool canJump(vector<int>& nums) {
-      int n=nums.size();
-      int lastend=0;
-      for(int i=0;i<n;i++){
-        if(lastend<i) return false;
-        lastend=max(lastend,nums[i]+i);
-        if(lastend>=n-1) return true;
-      } 
-      return true; 
+        dp.assign(nums.size() + 1, -1);
+        return solve(nums, 0);
     }
 };
