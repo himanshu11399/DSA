8dda133417ca257e07025eb2c38ea4004ead8c42
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-06-05 14:58:25 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (64.38%) - LeetSync

--- BinarySearchTree/274-h-index/h-index.cpp
+++ BinarySearchTree/274-h-index/h-index.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
-    int hIndex(vector<int>& citations) {
-        sort(citations.begin(), citations.end());
-        int n=citations.size();
-        int idx=0;
-        while(idx<n && citations[idx]<n-idx){
-             idx++;
-        }return n-idx;
+    int hIndex(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        int left=0;
+        int right=n-1;
+        while(left<=right){
+            int mid=(left+right)/2;
+            int quantity=n-mid;
+            if(nums[mid]>=quantity){
+                right=mid-1;
+            }else{
+                left=mid+1;
+            }
+        }
+        return  n-left;
     }
 };
