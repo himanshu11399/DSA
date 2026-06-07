e894317c609383d0f0b2b53384561faf57e37443
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-06-08 00:55:25 +0530

    Time: 1 ms (22.63%) | Memory: 27.4 MB (16.24%) - LeetSync

--- BinarySearchTree/42-trapping-rain-water/trapping-rain-water.cpp
+++ BinarySearchTree/42-trapping-rain-water/trapping-rain-water.cpp
@@ -2,32 +2,34 @@ class Solution {
 public:
     int trap(vector<int>& nums) {
         int n = nums.size();
-        if (n == 0) return 0;
+        vector<int> leftmax(n, 0);
+        vector<int> rightmax(n, 0);
 
-        vector<int> lmax(n, 0);
-        vector<int> rmax(n, 0);
-
-        // fill lmax (strictly left)
-        int leftmax = nums[0];
-        for (int i = 1; i < n; i++) {
-            lmax[i] = leftmax;
-            leftmax = max(leftmax, nums[i]);
-        }
-
-        // fill rmax (strictly right)
-        int rightmax = nums[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            rmax[i] = rightmax;
-            rightmax = max(rightmax, nums[i]);
-        }
-
-        int maxwater = 0;
+        // Fill leftmax
+        int left = 0;
         for (int i = 0; i < n; i++) {
-            int water = min(lmax[i], rmax[i]) - nums[i];
-            if (water > 0)
-                maxwater += water;
+            if (nums[i] < left) {
+                leftmax[i] = left;
+            }
+            left = max(left, nums[i]);
         }
 
-        return maxwater;
+        int right = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (nums[i] < right) {
+                rightmax[i] = right;
+            }
+            right = max(right, nums[i]);
+        }
+
+        int ans=0;
+        for(int i=0;i<n;i++){
+            int water=min(leftmax[i],rightmax[i]);
+            if(water>0){
+                water-=nums[i];
+            }
+            ans+=water;
+        }
+        return ans;
     }
 };
