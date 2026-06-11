9f482db592675579dd7c4539b4e4834f51e47b20
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-06-12 01:17:53 +0530

    Time: 636 ms (17.98%) | Memory: 0B (100.00%) - LeetSync

--- BinarySearchTree/180-consecutive-numbers/consecutive-numbers.sql
+++ BinarySearchTree/180-consecutive-numbers/consecutive-numbers.sql
@@ -1,11 +1,10 @@
 # Write your MySQL query statement below
-select Distinct num as ConsecutiveNums
-from (
-select 
-num,
-LAG(num,1) over (order by id) as prev1,
-LEAD(num,1) over (order by id) as next1
-from Logs
-) as t
-where num=prev1 && num=next1;
+select Distinct l1.num as ConsecutiveNums
+from Logs as l1
+join Logs as l2
+on l1.id-1=l2.id
+join Logs as l3
+on l1.id+1=l3.id
+where
+l1.num=l2.num && l1.num=l3.num;
 
