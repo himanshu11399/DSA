77aac6d9d48d118df04c658ad05bc1abbb20996d
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-06-14 23:54:21 +0530

    Time: 6 ms (34.27%) | Memory: 26 MB (5.73%) - LeetSync

--- BinarySearchTree/36-valid-sudoku/valid-sudoku.cpp
+++ BinarySearchTree/36-valid-sudoku/valid-sudoku.cpp
@@ -1,57 +1,23 @@
 class Solution {
 public:
-    bool validate(vector<vector<char>>& board, int sr, int sc, int er, int ec) {
-        unordered_set<char> st;
-        for (int i = sr; i <= er; i++) {
-            for (int j = sc; j <= ec; j++) {
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_set<string> st;
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
                 if (board[i][j] == '.')
                     continue;
-                if (st.count(board[i][j])) {
-                    return false;
-                }
-                st.insert(board[i][j]);
-            }
-        }
-        return true;
-    }
-    bool isValidSudoku(vector<vector<char>>& board) {
-        int n = board.size();
-        int m = board[0].size();
+                string row = string(1, board[i][j]) + "_ROW_" + to_string(i);
+                string col = string(1, board[i][j]) + "_COL_" + to_string(j);
+                string box = string(1, board[i][j]) + "_BOX_" +
+                             to_string(i / 3) + "_" + to_string(j / 3);
 
-        // Validate Row
-        for (int row = 0; row < 9; row++) {
-            unordered_set<char> st;
-            for (int col = 0; col < 9; col++) {
-                if (board[row][col] == '.')
-                    continue;
-                if (st.count(board[row][col])) {
+                if (st.count(row) || st.count(col) || st.count(box)) {
                     return false;
                 }
-                st.insert(board[row][col]);
-            }
-        }
-
-        // Validate col
-        for (int col = 0; col < 9; col++) {
-            unordered_set<char> st;
-            for (int row = 0; row < 9; row++) {
-                if (board[row][col] == '.')
-                    continue;
-                if (st.count(board[row][col])) {
-                    return false;
-                }
-                st.insert(board[row][col]);
-            }
-        }
-
-        // Validate Boxes
-        for (int sr = 0; sr < 9; sr += 3) {
-            int er = sr + 2;
-            for (int sc = 0; sc < 9; sc += 3) {
-                int ec = sc + 2;
-                if (!validate(board, sr, sc, er, ec)) {
-                    return false;
-                }
+                st.insert(row);
+                st.insert(col);
+                st.insert(box);
             }
         }
         return true;
