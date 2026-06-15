d1169b638a74d93ee2dde9d0b7adb1908d002bee
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-06-15 22:53:44 +0530

    Time: 15 ms (69.95%) | Memory: 26.1 MB (48.64%) - LeetSync

---
+++ BinarySearchTree/49-group-anagrams/group-anagrams.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mpp;
+
+        for (auto it : strs) {
+            string s = it;
+            sort(s.begin(), s.end());
+            mpp[s].push_back(it);
+        }
+
+        vector<vector<string>> ans;
+        for (auto it : mpp) {
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};
