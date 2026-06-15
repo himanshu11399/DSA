8925232243e2b248370c586f945321a1d01fa73e
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-06-15 23:57:15 +0530

    Time: 79 ms (39.29%) | Memory: 98.8 MB (13.87%) - LeetSync

--- BinarySearchTree/219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ BinarySearchTree/219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        unordered_set<int> st;
-
+        unordered_map<int, int> mpp;
         for (int i = 0; i < nums.size(); i++) {
-            if (st.count(nums[i])) {
-                return true;
+            if (mpp.find(nums[i]) != mpp.end()) {
+                if (abs(mpp[nums[i]] - i) <= k) {
+                    return true;
+                }
             }
-
-            st.insert(nums[i]);
-
-            if(st.size()>k){
-                st.erase(nums[i-k]);
-            }
+                mpp[nums[i]] = i;
+            
         }
-
         return false;
     }
 };
