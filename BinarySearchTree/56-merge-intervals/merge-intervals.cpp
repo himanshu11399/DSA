32170acf62e159cbb9250659a9d6235eac78e255
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-06-17 14:22:00 +0530

    Time: 3 ms (89.35%) | Memory: 23.8 MB (81.90%) - LeetSync

--- BinarySearchTree/56-merge-intervals/merge-intervals.cpp
+++ BinarySearchTree/56-merge-intervals/merge-intervals.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        int n = intervals.size();
         vector<vector<int>> ans;
 
-        sort(intervals.begin(),intervals.end());
+        sort(intervals.begin(),intervals.end(),
+           [](const vector<int>&a,const vector<int>&b){
+            return a[0] < b[0];
+           }
+        );
 
-        int st = intervals[0][0];
-        int end = intervals[0][1];
-        int i = 1;
-        while (i < intervals.size()) {
-            if (end >= intervals[i][0]) {
-                st = min(st, intervals[i][0]);
-                end = max(end, intervals[i][1]);
-                i++;
-            } else {
-                ans.push_back({st, end});
-                st = intervals[i][0];
-                end = intervals[i][1];
-                i++;
+         int st = intervals[0][0];
+        int en = intervals[0][1];
+
+        for(int i=1;i<intervals.size();i++){
+        int first = intervals[i][0];
+        int last = intervals[i][1];
+        if (en >= first) {
+            if(en>=last){
+                continue;
             }
+            en = last;
+        } else {
+            ans.push_back({st, en});
+            st = first;
+            en = last;
         }
-        ans.push_back({st, end});
-        return ans;
     }
-};
+    ans.push_back({st, en});
+    return ans;
+}
+}
+;
