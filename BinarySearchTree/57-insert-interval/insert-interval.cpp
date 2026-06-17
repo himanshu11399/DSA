bc909b8a9abe36f67ac4949e076987d889c10ad2
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-06-18 00:18:32 +0530

    Time: 4 ms (16.92%) | Memory: 21.7 MB (75.22%) - LeetSync

--- BinarySearchTree/57-insert-interval/insert-interval.cpp
+++ BinarySearchTree/57-insert-interval/insert-interval.cpp
@@ -1,27 +1,34 @@
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& interval,
+    vector<vector<int>> insert(vector<vector<int>>& intervals,
                                vector<int>& newInterval) {
+        if (intervals.empty()) {
+            return {{newInterval[0], newInterval[1]}};
+        }
         int i = 0;
+        int st = newInterval[0];
+        int en = newInterval[1];
         vector<vector<int>> ans;
-        while (i < interval.size()) {
-            if (interval[i][1] < newInterval[0]) {
-                ans.push_back({interval[i][0], interval[i][1]});
+
+        while (i < intervals.size()) {
+            if (intervals[i][1] < st) {
+                ans.push_back({intervals[i][0], intervals[i][1]});
                 i++;
-            } else if (interval[i][0] > newInterval[1]) {
-                ans.push_back({newInterval[0], newInterval[1]});
-                while(i<interval.size()){
-                ans.push_back({interval[i][0], interval[i][1]});
-                i++;
+            } else if (intervals[i][0] > en) {
+                ans.push_back({st, en});
+                while (i < intervals.size()) {
+                    ans.push_back({intervals[i][0], intervals[i][1]});
+                    i++;
                 }
                 return ans;
             } else {
-                newInterval[0] = min(newInterval[0], interval[i][0]);
-                newInterval[1] = max(newInterval[1], interval[i][1]);
+                st = min(st, intervals[i][0]);
+                en = max(en, intervals[i][1]);
                 i++;
             }
         }
-        ans.push_back({newInterval[0], newInterval[1]});
-        return ans;
+        ans.push_back({st, en});
+
+            return ans;
     }
 };
