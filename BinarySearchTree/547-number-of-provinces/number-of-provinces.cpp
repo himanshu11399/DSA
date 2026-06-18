0aaaf34047d9a5230aae07326168eea014cfa990
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-06-18 11:36:30 +0530

    Time: 4 ms (25.40%) | Memory: 21 MB (12.50%) - LeetSync

--- BinarySearchTree/547-number-of-provinces/number-of-provinces.cpp
+++ BinarySearchTree/547-number-of-provinces/number-of-provinces.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
-    void bfs(vector<vector<int>>& adj, vector<int>& vis, int& src) {
-        vis[src] = 1;
+    void dfs(vector<vector<int>>& adj, vector<bool>& vis, int src) {
+        vis[src] = true;
 
-        for (int i = 0; i < adj[src].size(); i++) {
-            int v = adj[src][i];
-            if (vis[v] == 0) {
-                bfs(adj, vis, v);
+        for(int i=0;i<adj[src].size();i++){
+            int v=adj[src][i];
+            if(!vis[v]){
+                dfs(adj,vis,v);
             }
         }
     }
+
     int findCircleNum(vector<vector<int>>& nums) {
-        int n = nums.size();
-        vector<vector<int>> adj(n);
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (nums[i][j] == 1 && i != j) {
+        vector<vector<int>> adj(nums.size());
+
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = 0; j < nums[i].size(); j++) {
+                if (nums[i][j] == 1) {
                     adj[i].push_back(j);
                     adj[j].push_back(i);
                 }
             }
         }
 
-        vector<int> vis(n, 0);
+        vector<bool> vis(nums.size() + 1, 0);
+
         int count = 0;
-        for (int i = 0; i < n; i++) {
-            if (vis[i] == 0) {
-                bfs(adj, vis, i);
+        for (int i = 0; i < nums.size(); i++) {
+            if (!vis[i]) {
+                dfs(adj, vis, i);
                 count++;
             }
         }
