555d14b9e4c0b1afba8dfe28aa7a1f3b2e1d2338
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-06-26 23:23:29 +0530

    Time: 0 ms (100.00%) | Memory: 21.9 MB (73.60%) - LeetSync

--- BinarySearchTree/98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ BinarySearchTree/98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -6,25 +6,21 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    bool isVerify(TreeNode* root, TreeNode*min, TreeNode*max) {
-        if (!root)
-            return true;
-        if (min && root->val <= min->val)
-            return false;
-        if (max && root->val >= max->val)
-            return false;
-        return isVerify(root->left, min, root) &&
-               isVerify(root->right, root, max);
+bool isValid(TreeNode*root,long long leftmax,long long rightmax){
+    if(!root){
+        return true;
     }
+    if(root->val <= leftmax || root->val >= rightmax){
+        return false;
+    }
+   return isValid(root->left,leftmax,root->val) && isValid(root->right,root->val,rightmax);
+}
     bool isValidBST(TreeNode* root) {
-        if (!root)
-            return true;
-        return isVerify(root,NULL,NULL);
+      return isValid(root,LLONG_MIN,LLONG_MAX);  
     }
 };
