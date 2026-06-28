4993db8b649ede1c4aa7f52913a1f1d52f119f26
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-06-28 22:33:15 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (10.50%) - LeetSync

--- BinarySearchTree/130-surrounded-regions/surrounded-regions.cpp
+++ BinarySearchTree/130-surrounded-regions/surrounded-regions.cpp
@@ -1,48 +1,48 @@
 class Solution {
 public:
-    void solve(vector<vector<char>>& mat) {
-       int n=mat.size();
-       int m=mat[0].size();
-
-       vector<vector<int>>vis(n,vector<int>(m,0));
-       vector<vector<int>>dir={{0,1},{1,0},{-1,0},{0,-1}};
-       queue<pair<int,int>>q;
-
-
-       for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-           if(i==0 || i==n-1 || j==0 || j==m-1){
-            if(mat[i][j]=='O'){
-                q.push({i,j});
-                vis[i][j]=1;
-            }
-           }
-        }
-       }
-
-       while(!q.empty()){
-        auto [row,col]=q.front();
-        q.pop();
-
-        for(auto it:dir){
-            int nrow=row+it[0];
-            int ncol=col+it[1];
-
-            if(nrow<n && nrow>=0 && ncol<m && ncol>=0 
-             && vis[nrow][ncol]==0 && mat[nrow][ncol]=='O'){
-               vis[nrow][ncol]=1;
-               q.push({nrow,ncol});
-             }
-        }
-       }
-
-       for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            if(vis[i][j]==0 && mat[i][j]=='O'){
-                mat[i][j]='X';
-                vis[i][j]=1;
+    vector<vector<int>> dir = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
+    void dfs(vector<vector<char>>& board, int sr, int sc,
+             vector<vector<bool>>& vis) {
+        vis[sr][sc] = true;
+        for (auto it : dir) {
+            int nrow = sr + it[0];
+            int ncol = sc + it[1];
+            if (nrow >= 0 && nrow < board.size() && ncol >= 0 &&
+                ncol < board[0].size() - 1 && !vis[nrow][ncol] &&
+                board[nrow][ncol] == 'O') {
+                dfs(board, nrow, ncol, vis);
             }
         }
-       }
+    }
+    void solve(vector<vector<char>>& board) {
+        int n = board.size();
+        int m = board[0].size();
+
+        queue<pair<int, int>> q;
+        vector<vector<bool>> vis(n + 1, vector<bool>(m, false));
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (i == 0 || i == n - 1 || j == 0 || j == m - 1) {
+                    if (board[i][j] == 'O') {
+                        q.push({i, j});
+                    }
+                }
+            }
+        }
+
+        while (!q.empty()) {
+            auto [row, col] = q.front();
+            q.pop();
+            dfs(board, row, col, vis);
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] == 'O' && vis[i][j] == false) {
+                    board[i][j] = 'X';
+                }
+            }
+        }
     }
 };
