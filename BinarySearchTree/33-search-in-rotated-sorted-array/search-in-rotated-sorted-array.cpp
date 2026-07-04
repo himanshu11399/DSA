c75da6aa4cd248ef36156d4974579a02c6d60ff0
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-07-05 00:09:53 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (34.49%) - LeetSync

--- BinarySearchTree/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ BinarySearchTree/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-     int low=0;
-     int high=nums.size()-1;
+        int left = 0;
+        int right = nums.size() - 1;
 
-     while(low<=high){
-        int mid=(low+high)/2;
-
-        if(nums[mid]==target){
-            return mid;
+        while (left <= right) {
+            int mid = left +(right-left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && nums[mid]>target) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[right] >= target && nums[mid]<target) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
         }
-
-        //If the left half us sorted
-        if(nums[low]<=nums[mid]){
-          if(nums[low]<=target && target<=nums[mid]){
-            high=mid-1;
-          }else{
-            low=mid+1;
-          }
-        }
-        else{  //Right part is sorted
-          if(nums[mid]<=target && target<=nums[high]){
-            low=mid+1;
-          }else{
-            high=mid-1;
-          }
-        }
-     }
-     return -1; 
+        return -1;
     }
 };
