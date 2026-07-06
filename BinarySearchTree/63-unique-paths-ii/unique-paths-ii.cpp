92c46b969a072cd561fa9b251e45c6289c437b23
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-07-07 00:36:11 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (6.20%) - LeetSync

--- BinarySearchTree/63-unique-paths-ii/unique-paths-ii.cpp
+++ BinarySearchTree/63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> dp;
-    int solve(vector<vector<int>>& nums, int i, int j) {
-        if (i >= nums.size() || j >= nums[0].size() || nums[i][j]==1) {
-            return 0;
-        }
-        if (i == nums.size() - 1 && j == nums[0].size() - 1) {
+    int solve(vector<vector<int>>& nums, int row, int col) {
+        if (row == nums.size() - 1 && col == nums[0].size() - 1 && nums[row][col]==0) {
             return 1;
         }
-        if (dp[i][j] != -1) {
-            return dp[i][j];
+        if (row >= nums.size() || col >= nums[0].size() ||
+            nums[row][col] == 1) {
+            return 0;
         }
-
-        int right = solve(nums, i, j + 1);
-        int bottom = solve(nums, i + 1, j);
-
-        return dp[i][j] = right + bottom;
+        if (dp[row][col] != -1) {
+            return dp[row][col];
+        }
+        int down = solve(nums, row + 1, col);
+        int right = solve(nums, row, col + 1);
+        return dp[row][col] =down+right;
     }
     int uniquePathsWithObstacles(vector<vector<int>>& nums) {
         int n = nums.size();
         int m = nums[0].size();
-        dp.assign(n, vector<int>(m, -1));
+        dp.assign(n + 1, vector<int>(m + 1, -1));
         return solve(nums, 0, 0);
     }
 };
