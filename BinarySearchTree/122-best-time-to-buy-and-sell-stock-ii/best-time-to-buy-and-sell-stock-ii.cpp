c607c46b001458c291b7b4c57bbee7ee67a1d371
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-07-15 12:14:16 +0530

    Time: 18 ms (5.30%) | Memory: 27.4 MB (17.96%) - LeetSync

--- BinarySearchTree/122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ BinarySearchTree/122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> dp;
-    int solve(vector<int>& nums, int idx, int buy) {
-        if (idx >= nums.size())
+    int solve(vector<int>& prices, int buy, int i) {
+        if (i >= prices.size()) {
             return 0;
-
-        if (dp[idx][buy] != -1) {
-            return dp[idx][buy];
+        }
+        if (dp[i][buy] != -1) {
+            return dp[i][buy];
         }
 
-        int take = 0;
-        int notake = 0;
-
-        if (buy == 1) {
-            take = -nums[idx] + solve(nums, idx + 1, 0);
-            notake = solve(nums, idx + 1, 1);
+        int profit = 0;
+        if (buy==1) {
+            int take = -prices[i]+solve(prices, 0, i + 1);
+            int notake = solve(prices, 1, i + 1);
+            profit = max(take, notake);
         } else {
-            take = nums[idx] + solve(nums, idx + 1, 1);
-            notake = solve(nums, idx + 1, 0);
+            int take = prices[i] + solve(prices, 1, i + 1);
+            int notake = solve(prices, 0, i + 1);
+            profit = max(take, notake);
         }
-
-        return dp[idx][buy] = max(take, notake);
+        return dp[i][buy] = max(profit,0);
     }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
         dp.assign(n + 1, vector<int>(2, -1));
-        return solve(prices, 0, 1);
+        return solve(prices, 1, 0);
     }
 };
