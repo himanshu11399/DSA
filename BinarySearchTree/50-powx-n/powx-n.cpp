6f00ebddc8694170d5ac50de501acee02f8078aa
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-07-16 21:52:21 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (70.57%) - LeetSync

--- BinarySearchTree/50-powx-n/powx-n.cpp
+++ BinarySearchTree/50-powx-n/powx-n.cpp
@@ -1,12 +1,23 @@
 class Solution {
 public:
-   double solve(double x,long n){
-    if(n==0) return 1;   //When the n is null 
-    if(n<0) return solve(1/x,-n);  //when the pow is neagtive use binary Exponential  Method
-    if(n%2==0) return solve(x*x,n/2);  //when the pow is even
-    return x*solve(x*x,(n-1)/2);  //when the pow is odd
-   }
+    double power(double x,long long n){
+        if(n==0) return 1;
+        if(n==1) return x;
+        if(n%2!=0){
+            return x*power(x,n-1);
+        }
+        double half=power(x,n/2);
+        return half*half;
+    }
     double myPow(double x, int n) {
-       return solve(x,(long)n); //call the function
+        double ans = 0;
+        long long N=n;
+        if (n < 0) {
+            N=-N;
+            ans = 1/power(x, N);
+        } else {
+            ans = power(x, n);
+        }
+        return ans;
     }
 };
