958cf12b5cc629f0d97967ca975000d471c095cd
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-08-13 11:05:09 +0530

    Time: 3 ms (40.71%) | Memory: 12.7 MB (76.90%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        int n1=nums1.size();
-       stack<int>st;
-       vector<int>ans;
-       unordered_map<int,int>mpp;
+        unordered_map<int, int> mpp;
+        stack<int> st;
+        vector<int> ans(nums1.size(), -1);
 
-       for(int i=nums2.size()-1;i>=0;i--){
-         while(!st.empty() && st.top()<nums2[i]){
-            st.pop();
-         }
-
-         if(st.size()<=0){
-            mpp[nums2[i]]=-1;
-         }else{
-            mpp[nums2[i]]=st.top();
-         }
-         st.push(nums2[i]);
-       } 
-        
-        for(int i=0;i<n1;i++){
-          ans.push_back(mpp[nums1[i]]);
+        for (int i = 0; i < nums2.size(); i++) {
+            while (!st.empty() && nums2[st.top()] < nums2[i]) {
+                mpp[nums2[st.top()]] = nums2[i];
+                st.pop();
+            }
+            st.push(i);
         }
-    return ans;
+
+        for (int i = 0; i < nums1.size(); i++) {
+            if (mpp.find(nums1[i]) != mpp.end()) {
+                ans[i] = mpp[nums1[i]];
+            }
+        }
+        return ans;
     }
 };
