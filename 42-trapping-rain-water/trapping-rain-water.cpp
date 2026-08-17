bf2a5a01832f75ac6781bbb9378ca89830f185f0
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-08-17 19:07:42 +0530

    Time: 2 ms (18.76%) | Memory: 27.3 MB (26.60%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
-    int trap(vector<int>& h) {
-       int ans=0;
-       int l=0;
-       int r=h.size()-1;
-       int lmax=0,rmax=0;
-       while(l<r){
-        lmax=max(lmax,h[l]);
-        rmax=max(rmax,h[r]);
-        if(lmax<rmax){
-            ans+=lmax-h[l];
-            l++;
-        }else{
-            ans+=rmax-h[r];
-            r--;
+    int trap(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> prefix(n, 0);
+        vector<int> suffix(n, 0);
+
+        int pre = 0;
+
+        for (int i = 0; i < n; i++) {
+            prefix[i] = max(nums[i], pre);
+            pre = max(pre, nums[i]);
         }
-       } 
-       return ans;
+
+        int suf = 0;
+
+        for (int j = n - 1; j >= 0; j--) {
+            suffix[j] = max(suf, nums[j]);
+            suf = max(suf, nums[j]);
+        }
+
+        int maxans = 0;
+        for (int i = 0; i < n; i++) {
+            maxans += min(prefix[i], suffix[i]) - nums[i];
+        }
+        return maxans;
     }
 };
