bfb5a2433f5ba9d96d27beffa855253a7600f5e2
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-08-27 01:08:26 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (37.37%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
+        int n = s.size();
 
+        stack<char> st;
         for (int i = 0; i < s.size(); i++) {
-            if (s[i] == '(' || s[i] == '[' || s[i] == '{') {
-                st.push(s[i]);
+            char ch = s[i];
+            if (ch == '(' || ch == '[' || ch == '{') {
+                st.push(ch);
             } else {
-                if (st.empty())
+                if (st.empty()) {
                     return false;
-                if ((s[i] == '}' && st.top() == '{') ||
-                    (s[i] == ']' && st.top() == '[') ||
-                    (s[i] == ')' && st.top() == '(')) {
+                } else if ((st.top() == '(' && ch == ')') ||
+                           (st.top() == '[' && ch == ']') ||
+                           st.top() == '{' && ch == '}') {
                     st.pop();
                 } else {
                     return false;
                 }
             }
         }
-        if(!st.empty()) return false;
+        if (!st.empty())
+            return false;
         return true;
     }
 };
