b56aa0e87745e668872b8831333670473ef8e212
Himanshu Sharma <himanshusharma12283@gmail.com>
2026-09-16 02:14:30 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (46.33%) - LeetSync

---
+++ 198-house-robber/house-robber.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> dp;
+    int solve(vector<int>& nums, int idx) {
+        if (idx >= nums.size()) {
+            return 0;
+        }
+        if (dp[idx] != -1)
+            return dp[idx];
+        int take = solve(nums, idx + 2) + nums[idx];
+        int notake = solve(nums, idx + 1);
+        return dp[idx] = max(take, notake);
+    }
+    int rob(vector<int>& nums) {
+        dp.assign(nums.size() + 1, -1);
+        return solve(nums, 0);
+    }
+};
